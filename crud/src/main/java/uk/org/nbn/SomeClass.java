3fd749f77b39830686f3deb1620679b8dfe8832b
sumothecat <sumo.the.cat@gmail.com>
2012-07-12 12:38:31 +0100

    add a couple of maven projects

---
+++ crud/src/main/java/uk/org/nbn/SomeClass.java
@@ -0,0 +1,8 @@
+
+package uk.org.nbn.nbnv;
+
+public class SomeClass {
+    public String doSomething(String s) {
+		return s + s;
+    }
+}
---
+++ importer/src/main/scala/uk/org/nbn/Importer.scala
@@ -0,0 +1,8 @@
+
+package uk.org.nbn.nbnv
+
+object Importer {
+    def main(args: Array[String]) {
+      println("Hello, world!")
+    }
+  }
