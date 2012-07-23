3c639f137549e46c405a972abaa29064b6cd13ba
Jonathan Cooper <jcoop@ceh.ac.uk>
2012-07-23 15:06:53 +0100

    NBNV-16 created initial web front end with designation page with sass and compass

---
+++ portal/src/main/java/uk/gov/nbn/data/powerless/PowerlessServlet.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.gov.nbn.data.powerless;
+
+import freemarker.ext.servlet.FreemarkerServlet;
+import freemarker.template.ObjectWrapper;
+import freemarker.template.TemplateModelException;
+import javax.servlet.ServletException;
+import uk.gov.nbn.data.powerless.json.JSONReaderForFreeMarker;
+
+/**
+ *
+ * @author Administrator
+ */
+public class PowerlessServlet extends FreemarkerServlet{
+    @Override
+    public void init() throws ServletException {
+        try {
+            super.init();
+            getConfiguration().setSharedVariable("markdown", new MarkDownDirectiveModel());
+            getConfiguration().setSharedVariable("json", new JSONReaderForFreeMarker());
+        } catch (TemplateModelException ex) {
+            throw new ServletException(ex);
+        }
+    }
+    
+    /*@Override
+    protected ObjectWrapper createObjectWrapper() {
+        return ObjectWrapper.DEFAULT_WRAPPER;
+    }*/
+    
+}
---
+++ portal/src/main/webapp/Designation_Category/index.ftl
@@ -0,0 +1,22 @@
+<#include "/WEB-INF/templates/master.ftl">
+
+<@master title="National Biodiversity Network Gateway">
+
+    <@markdown file="content.md" />
+    
+    <#assign designationCategories=json.readURL("http://localhost:8084/api/designationCategories")>
+    
+    <ul class="expandableList">
+        <#list designationCategories as currEntry>
+            <li>
+                <h1>${currEntry.label}</h1>
+                <ul>
+                <#assign designationCategory=json.readURL("http://localhost:8084/api/designationCategories/${currEntry.designationCategoryID}/designations")>
+                <#list designationCategory as currDesignation>
+                    <li><a href="/Designation?desig=${currDesignation.designationCategoryID}">${currDesignation.name}</a></li>
+                </#list>
+                </ul>
+            </li>
+        </#list>
+    </ul>
+</@master>
