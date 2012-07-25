05cf2a6145fdc47fb42707bce3dafc8c16a3327b
sumothecat <sumo.the.cat@gmail.com>
2012-07-25 12:19:01 +0100

    a real test with mocks; extract test data; add guice dependency

--- importer/src/test/scala/uk/org/nbn/nbnv/metadata/TestMetadataParserSuite.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/metadata/TestMetadataParserSuite.scala
@@ -8,41 +8,9 @@ import org.scalatest.matchers.ShouldMatchers
 @RunWith(classOf[JUnitRunner])
 class TestMetadataParserSuite extends FunSuite with ShouldMatchers {
   test("can parse valid metadata") {
-    val validXml = <eml xmlns="eml://ecoinformatics.org/eml-2.1.1" xmlns:dc="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" packageId="NBN/GA000159/eml-1.xml" scope="system" system="GBIF" xml:lang="en" xsi:schemaLocation="eml://ecoinformatics.org/eml-2.1.1 eml.xsd">
-      <dataset>
-        <alternateIdentifier>Test Identifier</alternateIdentifier>
-        <title xml:lang="en">Test Title</title>
-        <abstract>
-          <para>Test Description</para>
-        </abstract>
-        <intellectualRights>
-          <para>Access Constraints: Test Access constraints Use Constraints: Test Use Constraints</para>
-        </intellectualRights>
-        <coverage>
-          <geographicCoverage>
-            <geographicDescription>Test geographic coverage</geographicDescription>
-          </geographicCoverage>
-        </coverage>
-        <purpose>
-          <para>Test purpose</para>
-        </purpose>
-        <methods>
-          <methodStep>
-            <description>
-              <para>Test Method</para>
-            </description>
-          </methodStep>
-          <qualityControl>
-            <description>
-              <para>Test Quality</para>
-            </description>
-          </qualityControl>
-        </methods>
-      </dataset>
-    </eml>
 
     val parser = new MetadataParser()
-    val result = parser.parse(validXml)
+    val result = parser.parse(TestData.validMetadataXml)
 
     result.accessConstraints should be("Test Access constraints")
     result.datasetKey should be("Test Identifier")
---
+++ importer/src/test/scala/uk/org/nbn/nbnv/utility/TestFileSystemSuite.scala
@@ -0,0 +1,25 @@
+package uk.org.nbn.nbnv.utility
+
+package uk.org.nbn.nbnv
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.mockito.Mockito._
+
+@RunWith(classOf[JUnitRunner])
+class TestFileSystemSuite extends FunSuite with ShouldMatchers  {
+  // just checking i can do some mockin'
+  test("can mock with mockito") {
+    val fs = mock(classOf[FileSystem])
+    val xml = <hello>world</hello>
+    when(fs.loadXml("bob")).thenReturn(xml)
+
+    val result = fs.loadXml("bob")
+    result should be (xml)
+  }
+}
+
+
+
