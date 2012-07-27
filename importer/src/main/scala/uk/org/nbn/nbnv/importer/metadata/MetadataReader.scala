25407db031463c357793fb074facfcc1e184e933
sumothecat <sumo.the.cat@gmail.com>
2012-07-27 14:16:10 +0100

    tidy

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/metadata/MetadataReader.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/metadata/MetadataReader.scala
@@ -4,9 +4,11 @@ import org.gbif.dwc.text.Archive
 import uk.org.nbn.nbnv.utility.FileSystem
 
 class MetadataReader(fs: FileSystem, parser: MetadataParser) {
+
   def read(archive: Archive): Metadata = {
-    val file = archive.getMetadataLocationFile() // seems we can't just get the path
-    val path = file.getCanonicalPath()
+
+    val file = archive.getMetadataLocationFile // seems we can't just get the path
+    val path = file.getCanonicalPath
     val xml = fs.loadXml(path)
 
     parser.parse(xml)
