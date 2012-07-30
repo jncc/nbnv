c170578c35ad709b22f9eb58b7f61ade92e10379
felix mason <felix.mason@jncc.gov.uk>
2012-07-30 11:04:38 +0100

    queries for record ingester

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/data/RecordIngester.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/data/RecordIngester.scala
@@ -6,7 +6,7 @@
 package uk.org.nbn.nbnv.importer.data
 
 import uk.org.nbn.nbnv.jpa.nbncore._
-import javax.persistence.EntityManager
+import javax.persistence.{EntityManager, Query}
 import org.gbif.dwc.text.StarRecord
 import org.gbif.dwc.terms.DwcTerm;
 
@@ -14,9 +14,21 @@ import org.gbif.dwc.terms.DwcTerm;
 class RecordIngester (em : EntityManager) {
   
   def upsertRecord(record : StarRecord, dataset : Dataset) {
-    val survey : Option[Survey] = Some(em.find(classOf[Survey], record.core().value(DwcTerm.collectionCode)))
     
-    val sample : Option[Sample] = Some(em.find(classOf[Sample], record.core().value(DwcTerm.eventID)))
+    /*Query query = entityManager.createQuery("Select sm from SecureMessage sm where sm.someField=:arg1");
+     query.setParameter("arg1", arg1);*/
+
+    
+    val surveyQuery = em.createQuery("SELECT s FROM Survey WHERE s.surveyKey = :surveyKey AND s.datasetKey = :datasetKey", classOf[Survey])
+                      .setParameter("surveyKey", record.core().value(DwcTerm.collectionCode))
+                      .setParameter("datasetKey", dataset.getDatasetKey)
+    
+    val survey : Option[Survey] = Some(surveyQuery.getSingleResult())
+    
+    val sampleQuery = em.createQuery("SELECT s FROM Sample WHERE s.sampleKey=:sampleKey", classOf[Sample])
+                      .setParameter("sampleKey", record.core().value(DwcTerm.eventID))
+
+    val sample : Option[Sample] = Some(sampleQuery.getSingleResult())
     
     
     
