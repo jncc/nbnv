a7a815127ac9c64b46cacb692b294622caab2560
sumothecat <sumo.the.cat@gmail.com>
2012-08-07 11:03:26 +0100

    add sql server driver setup to readme

---
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/data/Implicits.scala
@@ -0,0 +1,13 @@
+package uk.org.nbn.nbnv.importer.data
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pete Montgomery
+ * Date: 07/08/12
+ * Time: 10:22
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Implicits {
+
+}
