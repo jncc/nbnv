6b27578965250c26689ac56c9c9ff863444ce3dc
Jonathan Cooper <jcoop@ceh.ac.uk>
2012-08-09 11:50:44 +0100

    TaxonGroups added to solr indexing

--- api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/TaxonResource.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/TaxonResource.java
@@ -1,20 +1,39 @@
 package uk.org.nbn.nbnv.api.rest.resources;
 
 import java.util.List;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.org.nbn.nbnv.api.dao.mappers.TaxonMapper;
 import uk.org.nbn.nbnv.api.model.Taxon;
+import uk.org.nbn.nbnv.api.solr.SolrResponse;
 
 @Component
 @Path("/species")
 public class TaxonResource {
     
     @Autowired TaxonMapper taxonMapper;
+    @Autowired SolrServer solrServer;
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public SolrResponse getTaxa(
+            @PathParam("id") String taxonGroup,
+            @QueryParam("rows") @DefaultValue("20") int rows,
+            @QueryParam("start") @DefaultValue("0") int start,
+            @QueryParam("q") String q
+            ) throws SolrServerException {
+        
+        SolrQuery query = new SolrQuery();
+        query.setQuery(q);
+        query.setFacet(true);
+        query.addFacetField("category");
+        query.setRows(rows);
+        query.setStart(start);
+        return new SolrResponse(solrServer.query(query));
+    }
     
 }
