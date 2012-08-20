16165b475a0cdd0ad763b402595ab26dc9408a85
Christopher Johnson <cjohn@ceh.ac.uk>
2012-08-20 16:11:30 +0100

    Adding a few extra comments to the Token Authenticator interface

--- api/src/main/java/uk/org/nbn/nbnv/api/authentication/TokenAuthenticator.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/authentication/TokenAuthenticator.java
@@ -8,9 +8,34 @@ import uk.org.nbn.nbnv.api.model.User;
  * @author Christopher Johnson
  */
 public interface TokenAuthenticator {
+    /**
+     * Generates a Token for a given username and password. Token will be valid
+     * for ttl milliseconds
+     * @param username
+     * @param password
+     * @param ttl Milliseconds for how long this token is required 
+     * @return A generated token for the user
+     * @throws InvalidCredentialsException
+     */
     public Token generateToken(String username, String password, int ttl) throws InvalidCredentialsException;
     
+    /**
+     * Generates a Token for a given username and md5 hash. Token will be valid
+     * for ttl milliseconds
+     * @param username
+     * @param md5_password the bytes of the hash to login with
+     * @param ttl Milliseconds for how long this token is required 
+     * @return A generated token for the user
+     * @throws InvalidCredentialsException
+     */
     public Token generateToken(String username, byte[] md5_password, int ttl) throws InvalidCredentialsException;
 
+    /**
+     * Gets the user associated with a particular token
+     * @param token The token of which to obtain the user for
+     * @return A user associated with the passed in token
+     * @throws InvalidTokenException If the token is not valid to this TokenAuthenticator
+     * @throws ExpiredTokenException If the token is no longer valid
+     */
     public User getUser(Token token) throws InvalidTokenException, ExpiredTokenException;
 }
