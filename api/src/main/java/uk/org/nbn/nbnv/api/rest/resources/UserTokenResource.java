182bda9d3f3adb9478b5e9e0ee3b6cc575a87a64
Christopher Johnson <cjohn@ceh.ac.uk>
2012-08-21 14:38:34 +0100

    Adding domain to cookie

--- api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/UserTokenResource.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/UserTokenResource.java
@@ -1,9 +1,11 @@
 package uk.org.nbn.nbnv.api.rest.resources;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
@@ -18,7 +20,7 @@ import uk.org.nbn.nbnv.api.rest.providers.TokenUserProvider;
 @Component
 @Path("/token")
 public class UserTokenResource {
-    private static final int DEFAULT_TOKEN_TTL = 1000;
+    private static final int DEFAULT_TOKEN_TTL = 30000;
     @Autowired TokenAuthenticator tokenAuth;
     
     @GET
@@ -35,11 +37,20 @@ public class UserTokenResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTaxonNavigationGroup(
             @QueryParam("username") String username, 
-            @QueryParam("password")String password
+            @QueryParam("password")String password,
+            @Context HttpServletRequest request
         ) throws InvalidCredentialsException {
         Token token = tokenAuth.generateToken(username, password, DEFAULT_TOKEN_TTL);
         return Response.ok()
-           .cookie(new NewCookie(TokenUserProvider.TOKEN_COOKIE_KEY, Base64.encodeBase64URLSafeString(token.getBytes())))
+           .cookie(new NewCookie(
+               TokenUserProvider.TOKEN_COOKIE_KEY, 
+               Base64.encodeBase64URLSafeString(token.getBytes()),
+                "/",
+                ".testnbn.net",
+                "authentication token",
+                DEFAULT_TOKEN_TTL/1000,
+                false
+            ))
            .build();
     }
 }
