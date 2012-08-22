2697a7cd4a08e869ed6c06e4618abc4b39a346c7
Christopher Johnson <cjohn@ceh.ac.uk>
2012-08-22 11:04:02 +0100

    Moving Nonce to begining of message

--- api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/UserResource.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/UserResource.java
@@ -1,11 +1,9 @@
 package uk.org.nbn.nbnv.api.rest.resources;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
