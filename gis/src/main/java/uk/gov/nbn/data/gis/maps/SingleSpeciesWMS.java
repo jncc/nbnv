e113fe6eed94aa9210d8dcb7da6bf46709c9943e
Christopher Johnson <cjohn@ceh.ac.uk>
2012-08-29 08:51:14 +0100

    Deploying single species map for testing on staging server

--- gis/src/main/java/uk/gov/nbn/data/gis/maps/SingleSpeciesWMS.java
+++ gis/src/main/java/uk/gov/nbn/data/gis/maps/SingleSpeciesWMS.java
@@ -1,6 +1,8 @@
 package uk.gov.nbn.data.gis.maps;
 
+import edu.umn.gis.mapscript.layerObj;
 import edu.umn.gis.mapscript.mapObj;
+import javax.servlet.http.HttpServletRequest;
 import uk.gov.nbn.data.gis.MapObject;
 import uk.gov.nbn.data.gis.MapService;
 import uk.gov.nbn.data.gis.Param;
@@ -11,10 +13,20 @@ import uk.gov.nbn.data.gis.Param;
  */
 @MapService("SingleSpecies")
 public class SingleSpeciesWMS {
-
+    
     @MapObject("{taxonVersionKey}")
-    public mapObj getTaxonMap(@Param("taxonVersionKey") String key) {
-        System.out.println(key);
-        return null;
+    public mapObj getTaxonMap(@Param("taxonVersionKey") String key,  HttpServletRequest request) {
+        mapObj toReturn = new mapObj(request.getRealPath("WEB-INF\\maps\\SingleSpeciesWMS.map"));
+        for(int i=0; i<toReturn.getNumlayers(); i++) {
+            layerObj layer = toReturn.getLayer(i);
+            layer.setData("geom from (SELECT geom, observationId, gridRef "
+                    + "FROM vw_SingleSpeciesMap WHERE resolutionID = " + (i+1)
+                    + "AND taxonVersionKey = '"+ key +"') "
+                    + "AS foo USING UNIQUE observationId USING SRID=4326");
+            
+        }
+        return toReturn;
     }
+    
+    
 }
