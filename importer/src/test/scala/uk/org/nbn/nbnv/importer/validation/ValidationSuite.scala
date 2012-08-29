f541c29ed0e046ce3b8c1850778d18453ec0e7fc
felix mason <felix.mason@jncc.gov.uk>
2012-08-29 17:22:42 +0100

    tvk validation tests

--- importer/src/test/scala/uk/org/nbn/nbnv/importer/validation/ValidationSuite.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/validation/ValidationSuite.scala
@@ -4,6 +4,7 @@ import uk.org.nbn.nbnv.importer.testing.BaseFunSuite
 import org.mockito.Mockito._
 import uk.org.nbn.nbnv.importer.records.NbnRecord
 import uk.org.nbn.nbnv.importer.fidelity.ResultLevel
+import uk.org.nbn.nbnv.importer.data.Repository
 
 
 class ValidationSuite extends BaseFunSuite {
@@ -43,6 +44,32 @@ class ValidationSuite extends BaseFunSuite {
     when(record.sampleKey).thenReturn("a" * 50)
 
     val v = new Nbnv63Validator
+    val r = v.validate(record)
+
+    r.level should be (ResultLevel.ERROR)
+  }
+
+  test("Nvnv64 should validate") {
+    val repo = mock[Repository]
+    when(repo.confirmTaxonVersionKey("some tvk")).thenReturn(true)
+
+    val record = mock[NbnRecord]
+    when(record.taxonVersionKey).thenReturn("some tvk")
+
+    val v = new Nbnv64Validator(repo)
+    val r = v.validate(record)
+
+    r.level should be (ResultLevel.DEBUG)
+  }
+
+  test("Nvnv64 should not validate") {
+    val repo = mock[Repository]
+    when(repo.confirmTaxonVersionKey("some tvk")).thenReturn(false)
+
+    val record = mock[NbnRecord]
+    when(record.taxonVersionKey).thenReturn("some tvk")
+
+    val v = new Nbnv64Validator(repo)
     val r = v.validate(record)
 
     r.level should be (ResultLevel.ERROR)
