e55c3e46e2e8bebaa22335373e39efb18da4e97d
Jonathan Cooper <jcoop@ceh.ac.uk>
2012-08-29 18:24:00 +0100

    these changes will conflict as i'm on a new vm, will fix

--- portal/src/main/webapp/js/enable-dataset-metadata-tabs.js
+++ portal/src/main/webapp/js/enable-dataset-metadata-tabs.js
@@ -1,8 +1,13 @@
 /* This javascript will implement JQuery tabs for a div with id=nbn-tabs
  * The html inside the div must be as specified in http://jqueryui.com/demos/tabs/ 
+ * It will also wrap up a table as a JQueryUI plugin DataTable provided it has the id 'nbn-generic-table'
+ * Dependencies:
+ * - /js/jquery.dataTables.min.js
+ * - http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/themes/smoothness/jquery-ui.css
  */
 (function($){
     $(document).ready(function(){
+        $.fn.dataTableExt.oJUIClasses.sStripeOdd = 'ui-state-highlight';
         $('#nbn-tabs').bind('tabsload', function(event, ui){
             $(ui.panel).find('#nbn-generic-datatable').dataTable({
             "bJQueryUI": true,
