6e4492cdc31de2b1f3bc43e940f379e0bad5fefb
felix mason <felix.mason@jncc.gov.uk>
2012-09-13 16:32:42 +0100

    Grid blurring implemented

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/spatial/BritishGridSquare.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/spatial/BritishGridSquare.scala
@@ -6,15 +6,20 @@ import com.sun.javaws.exceptions.InvalidArgumentException
 
 class BritishGridSquare(gridRef : String, precision: Int = 0) extends GridSquare {
 
+  //Check grid ref is uk grid ref
   if (gridRef.matches(GridRefPatterns.ukGridRef) == false
     && gridRef.matches(GridRefPatterns.ukDintyGridRef)  == false)
     throw new IllegalArgumentException("Grid reference '%s' is not a valid uk grid reference".format(gridRef))
+
+  //Check grid ref is not below minimum preciesion
+  val currentPrecision = getPrecision(gridRef)
+
+  if (currentPrecision > 10000) throw new IllegalArgumentException("Grid reference precision be low 10Km")
 
   //Normalise the precision to one of the allowable values
   val normalisedPrecision = normalisePrecision(precision)
 
   val outputGridRef = {
-    val currentPrecision = getPrecision(gridRef)
 
     if (normalisedPrecision < currentPrecision) {
       throw ImportFailedException("Normailised precsion '%s' is greater then grid ref '%s' precision".format(normalisedPrecision, gridRef))
@@ -30,6 +35,7 @@ class BritishGridSquare(gridRef : String, precision: Int = 0) extends GridSquare
     }
   }
 
+
   def gridReference = outputGridRef
 
   def gridReferencePrecision = getPrecision(outputGridRef)
@@ -38,21 +44,39 @@ class BritishGridSquare(gridRef : String, precision: Int = 0) extends GridSquare
 
   def getParentGridRef = null
 
-  private def decreaseGridPrecision(gridRef: String, targetPrecision: Int) = {
+  private def decreaseGridPrecision(gridRef: String, targetPrecision: Int) : String = {
     //If targetPrecision is 2000 decrease to DINTY grid ref
     if (targetPrecision == 2000) {
       computeDintyFromGridRef(gridRef)
     }
     //Else reduce to target grid ref
-    else if (gridRef.matches(GridRefPatterns.ukDintyGridRef)){
-      //todo: dinty grid ref precision reduction do something here
+    else if (gridRef.matches(GridRefPatterns.ukDintyGridRef) && targetPrecision == 10000){
+      //can only reduce this to 10000m
+      gridRef.substring(0,4)
     }
-    else {
-      //todo: reduce precision of a normal grid ref to a normal precision
+    else if (targetPrecision == 100){
+      trimGridDigits(gridRef, 6)
     }
+    else if (targetPrecision == 1000){
+      trimGridDigits(gridRef, 4)
+    }
+    else if (targetPrecision == 10000) {
+      trimGridDigits(gridRef, 2)
+    }
+    else
+    {
+      throw new IllegalArgumentException("Invalid target precision")
+    }
+  }
 
-    //todo: get rid of this null
-    null
+  private def trimGridDigits(gridRefString: String, maxDigits: Int) = {
+    var numericPart = getNumeralsFromGridRef(gridRef)
+    var parts = numericPart.splitAt(numericPart.length / 2)
+    var easting = parts._1.substring(0, maxDigits / 2)
+    var northing = parts._2.substring(0, maxDigits / 2)
+    var gridLetters = getlettersFromGridRef(gridRef)
+
+    gridLetters + easting + northing
   }
 
   private def computeDintyFromGridRef(gridRef: String) = {
@@ -74,7 +98,7 @@ class BritishGridSquare(gridRef : String, precision: Int = 0) extends GridSquare
       val dintyLetter = getDintyLeter(dintyEasting, dintyNorthing)
 
       //gives TL
-      val gridLetters = gridRef.substring(0,2)
+      val gridLetters = getlettersFromGridRef(gridRef)
       //gives 2
       val easting = numericComponents._1.substring(0,1)
       //gives 3
@@ -98,9 +122,13 @@ class BritishGridSquare(gridRef : String, precision: Int = 0) extends GridSquare
     }
     else {
       //Otherwise the precision is inversely proportional to ten to the power of the number of digits
-      var numerals = getNumeralsFromGridRef(gridRef).length
+      val numerals = getNumeralsFromGridRef(gridRef).length
       1000000 / 10 ^ (numerals / 2)
     }
+  }
+
+  private def getlettersFromGridRef(gridRef : String) = {
+    gridRef.substring(0,2)
   }
 
   private def getNumeralsFromGridRef(gridRef : String) = {
