3be9026298aa43693c7948d1219530253e001b58
Matt Debont <Matt Debont@Win7-64-MattD.green.jncc.gov.uk>
2012-09-14 13:57:40 +0100

    Finished logo upload functionality, should be encoding to Base64 and seems to work on image resizing (I feedback the images to the UI)

--- crud/src/main/java/uk/org/nbn/nbnv/jpa/nbncore/Organisation.java
+++ crud/src/main/java/uk/org/nbn/nbnv/jpa/nbncore/Organisation.java
@@ -34,6 +34,7 @@ public class Organisation implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     @Column(name = "organisationID")
     private Integer organisationID;
     @Basic(optional = false)
