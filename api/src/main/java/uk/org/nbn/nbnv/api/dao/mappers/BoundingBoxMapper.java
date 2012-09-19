69f2e0cffff4d56fbdff540988422a2e7ea04d1e
Christopher Johnson <cjohn@ceh.ac.uk>
2012-09-19 13:55:22 +0100

    Adding BoundingBox mapper to retrieve the bounds of a certain grid square

---
+++ api/src/main/java/uk/org/nbn/nbnv/api/dao/mappers/BoundingBoxMapper.java
@@ -0,0 +1,20 @@
+package uk.org.nbn.nbnv.api.dao.mappers;
+
+import org.apache.ibatis.annotations.Select;
+import uk.org.nbn.nbnv.api.model.BoundingBox;
+
+/**
+ * The following mapper will return bounding boxes for various features held
+ * on the NBN Gateway
+ * @author Christopher Johnson
+ */
+public interface BoundingBoxMapper {
+    @Select("SELECT " + 
+                "MIN(geom.STEnvelope().STPointN(1).STX) as minX, " + 
+                "MIN(geom.STEnvelope().STPointN(1).STY) as minY, " + 
+                "MAX(geom.STEnvelope().STPointN(3).STX) as maxX, " + 
+                "MAX(geom.STEnvelope().STPointN(3).STY) as maxY " + 
+            "FROM GridSquareFeatureData " + 
+            "WHERE featureID = #{id}")
+    BoundingBox getBoundingBoxForGridSquare(int id);
+}
