382a12ea86fd84281e611010956fca0550c19c38
Christopher Johnson <cjohn@ceh.ac.uk>
2012-09-24 16:01:54 +0100

    Removing unused servlets enabling siteboundary layer through get map url call

--- imt/src/main/javascript/layer/ResolvingVisibleLayersArcGisMapFilter.js
+++ imt/src/main/javascript/layer/ResolvingVisibleLayersArcGisMapFilter.js
@@ -11,27 +11,27 @@ window.nbn = window.nbn || {};
 nbn.layer = nbn.layer || {};
 
 nbn.layer.ResolvingVisibleLayersArcGisMapFilter = function(type, initial) {
-	var _underlyingFilter, _resolvedFilter;
-	$.extend(this, _underlyingFilter = new nbn.layer.ArcGisMapFilter());
+    var _underlyingFilter, _resolvedFilter;
+    $.extend(this, _underlyingFilter = new nbn.layer.ArcGisMapFilter());
 	
-	this.setFilter = function(filterToSet) {
-		if(filterToSet) {
-			nbn.util.EntityResolver.resolve(new function() {
-					this[type] = filterToSet;
-				},function(resolvedFilter) {
-					_resolvedFilter = resolvedFilter; //store the resolved object
-					_underlyingFilter.setFilter({
-						visibleLayers: (!$.isArray(resolvedFilter[type])) ? [resolvedFilter[type].gisLayerID] : nbn.util.ArrayTools.getArrayByFunction(resolvedFilter[type], function(element) {
-							return element.gisLayerID;
-						})
-					});
-			});
-		}
-	};
+    this.setFilter = function(filterToSet) {
+        if(filterToSet) {
+            nbn.util.EntityResolver.resolve(new function() {
+                this[type] = filterToSet;
+            },function(resolvedFilter) {
+                _resolvedFilter = resolvedFilter; //store the resolved object
+                _underlyingFilter.setFilter({
+                    visibleLayers: (!$.isArray(resolvedFilter[type])) ? [resolvedFilter[type].datasetKey] : nbn.util.ArrayTools.getArrayByFunction(resolvedFilter[type], function(element) {
+                        return element.datasetKey;
+                    })
+                });
+            });
+        }
+    };
 	
-	this.getResolvedFilter = function() {
-		return _resolvedFilter;
-	};
+    this.getResolvedFilter = function() {
+        return _resolvedFilter;
+    };
 	
-	this.setFilter(initial); //set the map for an initial call
+    this.setFilter(initial); //set the map for an initial call
 };
