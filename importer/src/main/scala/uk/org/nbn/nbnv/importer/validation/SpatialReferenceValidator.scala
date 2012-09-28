3083c0e34d9b2ade0aced01981fe6636feae8c47
felix mason <felix.mason@jncc.gov.uk>
2012-09-28 16:23:41 +0100

    Nbnv82Validation and spatial validation framework

---
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/validation/SpatialReferenceValidator.scala
@@ -0,0 +1,29 @@
+package uk.org.nbn.nbnv.importer.validation
+
+import uk.org.nbn.nbnv.importer.records.NbnRecord
+import collection.mutable.ListBuffer
+import uk.org.nbn.nbnv.importer.fidelity.{ResultLevel, Result}
+
+class SpatialReferenceValidator {
+  def validate(record: NbnRecord) = {
+    val resultList = new ListBuffer[Result]
+
+    val v1 = new Nbnv82Validator
+    val r1 = v1.validate(record)
+    resultList.append(r1)
+
+    if (r1.level == ResultLevel.DEBUG){
+      if (record.gridReference.isDefined) {
+        //todo: valdiate grid ref
+      }
+      else if (record.featureKey.isDefined) {
+        //todo: validate feature key
+      }
+      else if (record.east.isDefined && record.north.isDefined) {
+        //todo: validate point ?
+      }
+    }
+
+    resultList.toList
+  }
+}
