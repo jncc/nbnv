91542157f28e3891f0081fa1026cc2ac7e55d631
Christopher Johnson <cjohn@ceh.ac.uk>
2012-10-05 14:44:09 +0100

    Removing array from annotation as there is currently only one background layer defined

--- gis/src/main/java/uk/gov/nbn/data/gis/maps/SingleSpeciesWMS.java
+++ gis/src/main/java/uk/gov/nbn/data/gis/maps/SingleSpeciesWMS.java
@@ -58,7 +58,7 @@ public class SingleSpeciesWMS {
             @GridLayer(name="100m",     layer="Grid-100m",      resolution=Resolution.ONE_HUNDRED_METERS)
         },
         defaultLayer="10km",
-        backgrounds={@Layer(name="os", layer="OS-Scale-Dependant" ) },
+        backgrounds=@Layer(name="os", layer="OS-Scale-Dependant" ),
         overlays=@Layer(name="feature", layer="Selected-Feature" )
     )
     public MapFileModel getSingleSpeciesModel(
