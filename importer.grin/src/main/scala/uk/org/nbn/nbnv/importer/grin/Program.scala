8bbb153503a03eb99a640949a12c9fb90b7d67b8
sumothecat <sumo.the.cat@gmail.com>
2012-10-09 10:34:09 +0100

    mangle grin

--- importer.grin/src/main/scala/uk/org/nbn/nbnv/importer/grin/Program.scala
+++ importer.grin/src/main/scala/uk/org/nbn/nbnv/importer/grin/Program.scala
@@ -6,8 +6,14 @@ import injection.GuiceModule
 import uk.org.nbn.nbnv.importer.ingestion.FeatureIngester
 import javax.persistence.{EntityTransaction, EntityManager}
 import io.Source
-import org.apache.log4j.Logger
+import org.apache.log4j.{Level, Logger}
 import javax.validation.ConstraintViolationException
+import com.google.common.base.Stopwatch
+import uk.org.nbn.nbnv.importer.logging.Log
+import uk.org.nbn.nbnv.PersistenceUtility
+import uk.org.nbn.nbnv.importer.Settings
+import uk.org.nbn.nbnv.importer.data.{QueryCache, Repository}
+import uk.org.nbn.nbnv.importer.spatial.GridSquareInfoFactory
 
 object Program {
 
@@ -28,26 +34,43 @@ object Program {
 
   def create(options: Options) = {
 
-    val injector = Guice.createInjector(new GuiceModule(options))
-    injector.getInstance(classOf[Program])
+//    val injector = Guice.createInjector(new GuiceModule(options))
+//    injector.getInstance(classOf[Program])
+
+    Log.configure(options.logDir, "4MB", Level.INFO)
+    val log = Log.get()
+    new Program(log, options)
   }
 }
 
-class Program @Inject() (log: Logger, options: Options, em: EntityManager, ingester: FeatureIngester) {
+class Program @Inject() (log: Logger, options: Options) { // , em: EntityManager, ingester: FeatureIngester
 
   def run() {
 
-    val t = em.getTransaction
 
-    withTransaction(t, options.whatIf) {
+    val watch = new Stopwatch().start()
 
-      for {
-        line <- Source.fromFile(options.dataPath).getLines().take(10)
-        ref = line.trim
-        if !ref.isEmpty
-      } {
-        log.info("Importing grid ref '%s'".format(ref))
-        ingester.ensureGridRefFeature(ref)
+    val groupsOfRefs = (for {
+      line <- Source.fromFile(options.dataPath).getLines().drop(2000).take(5000)
+      ref = line.trim
+      if !ref.isEmpty
+    } yield ref).zipWithIndex.grouped(100)
+
+    for (g <- groupsOfRefs) {
+
+      val em = new PersistenceUtility().createEntityManagerFactory(Settings.map).createEntityManager
+      val repo = new Repository(log, em, new QueryCache(log))
+      val t = em.getTransaction
+      val ingester = new FeatureIngester(log, em, repo, new GridSquareInfoFactory)
+
+      withTransaction(t, options.whatIf) {
+
+        for ((ref, i) <- g) {
+          log.info("Importing grid ref '%s'".format(ref))
+          ingester.ensureGridRefFeature(ref)
+          log.info("Imported " + (i + 1) + " grid refs in " + watch.elapsedMillis() + "ms")
+          log.info("Average speed is " + watch.elapsedMillis() / (i + 1) + "ms per grid ref")
+        }
       }
     }
   }
@@ -81,9 +104,15 @@ class Program @Inject() (log: Logger, options: Options, em: EntityManager, inges
       }
     }
     finally {
-      em.close()
+      //em.close()
     }
   }
 
 }
 
+//truncate table gridsquare
+//truncate table featurecontains
+//truncate table FeatureOverlaps
+//truncate table GridTree
+//delete from feature
+//DBCC CHECKIDENT('Feature', RESEED, 0)
