04c893765400d0efbc51c1f3b5a213ecf8b57250
felix mason <felix.mason@jncc.gov.uk>
2012-10-17 13:51:01 +0100

    Nbnv90Validator

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/validation/GridReferenceValidator.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/validation/GridReferenceValidator.scala
@@ -3,8 +3,10 @@ package uk.org.nbn.nbnv.importer.validation
 import uk.org.nbn.nbnv.importer.records.NbnRecord
 import collection.mutable.ListBuffer
 import uk.org.nbn.nbnv.importer.fidelity.{ResultLevel, Result}
+import com.google.inject.Inject
+import uk.org.nbn.nbnv.importer.spatial.GridSquareInfoFactory
 
-class GridReferenceValidator  {
+class GridReferenceValidator @Inject()(factory: GridSquareInfoFactory) {
   def validate(record: NbnRecord) = {
     val resultList = new ListBuffer[Result]
 
@@ -19,6 +21,10 @@ class GridReferenceValidator  {
       val v2 = new Nbnv159Validator()
       val r2 = v2.validate(record.gridReference.get, record.gridReferenceType, record.key)
       resultList.append(r2)
+
+      val v3 = new Nbnv90Validator(factory)
+      val v3results = v3.validate(record)
+      for (result <- v3results) resultList.append(result)
     }
 
     resultList.toList
---
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/validation/Nbnv90ValidatorSuite.scala
@@ -0,0 +1,113 @@
+package uk.org.nbn.nbnv.importer.validation
+
+import uk.org.nbn.nbnv.importer.testing.BaseFunSuite
+import uk.org.nbn.nbnv.importer.spatial.{GridSquareInfo, GridSquareInfoFactory}
+import uk.org.nbn.nbnv.importer.records.NbnRecord
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+import uk.org.nbn.nbnv.importer.fidelity.ResultLevel
+
+class Nbnv90ValidatorSuite extends BaseFunSuite {
+
+  val knownGridRef_100m = "NN166712"
+  val knownGridRef_1000m = "NN1671"
+
+  test("should validate 100m grid ref reduced to precision of 1000m") {
+    val gsi = mock[GridSquareInfo]
+    when(gsi.gridReferencePrecision).thenReturn(100)
+
+    val factory = mock[GridSquareInfoFactory]
+    when(factory.getByGridRef(knownGridRef_100m)).thenReturn(gsi)
+
+    val record = mock[NbnRecord]
+    when(record.gridReference).thenReturn(Some(knownGridRef_100m))
+    when(record.gridReferencePrecision).thenReturn(1000)
+
+    val v = new Nbnv90Validator(factory)
+    val results = v.validate(record)
+    val result = results.find(r => r.level == ResultLevel.ERROR)
+
+    result should be (None)
+  }
+
+  test("should validate if no target precision is requested") {
+    val gsi = mock[GridSquareInfo]
+    when(gsi.gridReferencePrecision).thenReturn(100)
+
+    val factory = mock[GridSquareInfoFactory]
+    when(factory.getByGridRef(knownGridRef_100m)).thenReturn(gsi)
+
+    val record = mock[NbnRecord]
+    when(record.gridReference).thenReturn(Some(knownGridRef_100m))
+    when(record.gridReferencePrecision).thenReturn(0)
+
+    val v = new Nbnv90Validator(factory)
+    val results = v.validate(record)
+    val result = results.find(r => r.level == ResultLevel.ERROR)
+
+    result should be (None)
+  }
+
+  test("should not validate 1000m grid ref increased to 100m") {
+    val gsi = mock[GridSquareInfo]
+    when(gsi.gridReferencePrecision).thenReturn(1000)
+
+    val factory = mock[GridSquareInfoFactory]
+    when(factory.getByGridRef(knownGridRef_1000m)).thenReturn(gsi)
+
+    val record = mock[NbnRecord]
+    when(record.gridReference).thenReturn(Some(knownGridRef_1000m))
+    when(record.gridReferencePrecision).thenReturn(100)
+
+    val v = new Nbnv90Validator(factory)
+    val results = v.validate(record)
+    val result = results.find(r => r.level == ResultLevel.ERROR)
+
+    result should not be (None)
+  }
+
+  test("should warn but not error if target precision is less then 100 and grid ref precision is 100") {
+    val gsi = mock[GridSquareInfo]
+    when(gsi.gridReferencePrecision).thenReturn(100)
+
+    val factory = mock[GridSquareInfoFactory]
+    when(factory.getByGridRef(knownGridRef_100m)).thenReturn(gsi)
+
+    val record = mock[NbnRecord]
+    when(record.gridReference).thenReturn(Some(knownGridRef_100m))
+    when(record.gridReferencePrecision).thenReturn(50)
+
+    val v = new Nbnv90Validator(factory)
+    val results = v.validate(record)
+
+    val error = results.find(r => r.level == ResultLevel.ERROR)
+    error should be (None)
+
+    val warn = results.find(r => r.level == ResultLevel.WARN)
+    warn should not be (None)
+  }
+
+  test("should warn and error if target precision is less then 100 and grid ref precision is greater than 100") {
+    val gsi = mock[GridSquareInfo]
+    when(gsi.gridReferencePrecision).thenReturn(1000)
+
+    val factory = mock[GridSquareInfoFactory]
+    when(factory.getByGridRef(knownGridRef_1000m)).thenReturn(gsi)
+
+    val record = mock[NbnRecord]
+    when(record.gridReference).thenReturn(Some(knownGridRef_1000m))
+    when(record.gridReferencePrecision).thenReturn(50)
+
+    val v = new Nbnv90Validator(factory)
+    val results = v.validate(record)
+
+    val error = results.find(r => r.level == ResultLevel.ERROR)
+    error should not be (None)
+
+    val warn = results.find(r => r.level == ResultLevel.WARN)
+    warn should not be (None)
+  }
+
+
+
+}
