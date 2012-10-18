5083443a58cd685d5acdba2fc06a574aacf28785
felix mason <felix.mason@jncc.gov.uk>
2012-10-18 16:03:20 +0100

    nbnv88 tests

---
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/validation/Nbnv88ValidatorSuite.scala
@@ -0,0 +1,53 @@
+package uk.org.nbn.nbnv.importer.validation
+
+import uk.org.nbn.nbnv.importer.testing.BaseFunSuite
+import uk.org.nbn.nbnv.importer.records.NbnRecord
+import org.mockito.Mockito._
+import uk.org.nbn.nbnv.importer.data.{Repository, Database}
+import uk.org.nbn.nbnv.importer.fidelity.ResultLevel
+import org.scalatest.BeforeAndAfter
+
+class Nbnv88ValidatorSuite extends BaseFunSuite with BeforeAndAfter {
+  var record : NbnRecord = _
+  var db : Database = _
+  var repo : Repository = _
+
+  before {
+    record = mock[NbnRecord]
+    repo = mock[Repository]
+    db = mock[Database]
+
+    when(db.repo).thenReturn(repo)
+  }
+
+
+  test("should validate a valid FeatureKey") {
+    when(record.featureKey).thenReturn(Option("DS123456PK123"))
+    when(repo.confirmSite("DS123456","PK123")).thenReturn(true)
+
+    val v = new Nbnv88Validator(db)
+    var r = v.validate(record)
+
+    r.level should be (ResultLevel.DEBUG)
+  }
+
+  test("should not validate an invalid FeatureKey") {
+    when(record.featureKey).thenReturn(Option("DS123456PK123"))
+    when(repo.confirmSite("DS123456","PK123")).thenReturn(false)
+
+    val v = new Nbnv88Validator(db)
+    var r = v.validate(record)
+
+    r.level should be (ResultLevel.ERROR)
+  }
+
+  test("should not validate a FeatureKey that is less then 9 charachters") {
+    when(record.featureKey).thenReturn(Option("1"))
+
+    val v = new Nbnv88Validator(db)
+    var r = v.validate(record)
+
+    r.level should be (ResultLevel.ERROR)
+  }
+
+}
