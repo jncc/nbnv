6d36e8f4b2bc35ff4e6bc92110b8e653777feaeb
sumothecat <sumo.the.cat@gmail.com>
2012-10-25 11:52:47 +0100

    better error msg

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/utility/ResourceLoader.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/utility/ResourceLoader.scala
@@ -1,9 +1,11 @@
 package uk.org.nbn.nbnv.importer.utility
 
+import java.io.FileNotFoundException
+
 
 /// Loads a java resource from the package.
 
 trait ResourceLoader {
-  def resource(path: String) = getClass.getResource(path)
+  def resource(path: String) = Option(getClass.getResource(path)) getOrElse { throw new FileNotFoundException(path) }
 }
 
