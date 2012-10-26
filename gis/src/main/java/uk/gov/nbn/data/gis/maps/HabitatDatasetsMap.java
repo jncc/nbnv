7b47baa3d565bf27260736bd73da4fcb41d1f3ac
Christopher Johnson <cjohn@ceh.ac.uk>
2012-10-26 15:03:56 +0100

    Adding habitat details  to data api
    Adding a habitat dataset context layer to the gis application

--- gis/src/main/java/uk/gov/nbn/data/gis/maps/HabitatDatasetsMap.java
+++ gis/src/main/java/uk/gov/nbn/data/gis/maps/HabitatDatasetsMap.java
@@ -12,28 +12,28 @@ import uk.gov.nbn.data.gis.processor.MapFileModel;
 import uk.gov.nbn.data.gis.processor.MapService;
 import uk.gov.nbn.data.gis.processor.MapContainer;
 import uk.gov.nbn.data.gis.providers.annotations.ServiceURL;
-import uk.org.nbn.nbnv.api.model.SiteBoundaryDataset;
+import uk.org.nbn.nbnv.api.model.HabitatDataset;
 
 /**
  * The following map service will make a call to the data api as defined in
- * the gis.properties to retrieve the most upto-date list of site boundary 
+ * the gis.properties to retrieve the most upto-date list of habitat 
  * datasets. This will then be used to create the layers for the map file
  * @author Christopher Johnson
  */
 @Component
-@MapContainer("SiteBoundaryDatasets")
-public class SiteBoundaryDatasetsMap {
+@MapContainer("HabitatDatasets")
+public class HabitatDatasetsMap {
     @Autowired Properties properties;
     @Autowired WebResource dataApi;
     private final LayerGenerator layerGenerator = new LayerGenerator();
     
     private static class LayerGenerator {
         private static final String DATA = "geom from ("
-            + "SELECT geom, sbfd.id "
-            + "FROM SiteBoundaryFeatureData sbfd "
-            + "INNER JOIN SiteBoundaryData sbd ON sbd.featureID = sbfd.id "
-            + "WHERE siteBoundaryDatasetKey = '%s'"
-        + ") AS foo USING UNIQUE featureID USING SRID=4326";
+            + "SELECT geom, hffd.id "
+            + "FROM HabitatFeatureFeatureData hffd "
+            + "INNER JOIN HabitatFeatureData hfd ON hfd.featureID = hffd.id "
+            + "WHERE habitatDatasetKey = '%s'"
+        + ") AS foo USING UNIQUE id USING SRID=4326";
     
         public String getData(String siteBoundary) {
             return String.format(DATA, siteBoundary);
@@ -42,16 +42,16 @@ public class SiteBoundaryDatasetsMap {
     
     @MapService
     public MapFileModel getSiteBoundariesModel(@ServiceURL String mapServiceURL) {
-        List<SiteBoundaryDataset> datasets = dataApi
-                        .path("siteBoundaryDatasets")
+        List<HabitatDataset> datasets = dataApi
+                        .path("habitatDatasets")
                         .accept(MediaType.APPLICATION_JSON) 
-                        .get(new GenericType<List<SiteBoundaryDataset>>() { });
+                        .get(new GenericType<List<HabitatDataset>>() { });
         
         HashMap<String, Object> data = new HashMap<String, Object>();
         data.put("layerGenerator", layerGenerator);
         data.put("mapServiceURL", mapServiceURL);
         data.put("properties", properties);
-        data.put("siteBoundaries", datasets);
-        return new MapFileModel("SiteBoundaryDatasets.map",data);
+        data.put("habitats", datasets);
+        return new MapFileModel("HabitatDatasets.map",data);
     }
 }
