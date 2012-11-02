c6f21387b4b4a6b0e82842d2ec1a37917d0442ad
sumothecat <sumo.the.cat@gmail.com>
2012-11-02 12:18:46 +0000

    use common properties / settings file in importer

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/Settings.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/Settings.scala
@@ -8,35 +8,11 @@ import java.io.{InputStream, FileInputStream}
 
 object Settings {
 
-  val map = new Properties
-
-  withInputStream(this.getClass.getResourceAsStream("/settings.properties")) {
-    s => map.load(s)
-  }
-
-  // override with server-specific file on a conventional path
-  private val overridingFile = new java.io.File("/nbnv-settings/importer/settings.properties")
-
-  if (overridingFile.exists) {
-    val overriding = new Properties
-
-    withInputStream(new FileInputStream(overridingFile)) {
-      s => overriding.load(s)
-    }
-    map.putAll(overriding)
-  }
+  val map = uk.gov.nbn.data.properties.PropertiesReader.getEffectiveProperties("importer.properties")
 
   // provide statically-bound access to settings
   // ...
   def myTestSetting = map.getProperty("my.test.setting")
 
-  def withInputStream(s: InputStream)(f: InputStream => Unit) {
-    try {
-      f(s)
-    }
-    finally {
-      s.close()
-    }
-  }
 }
 
