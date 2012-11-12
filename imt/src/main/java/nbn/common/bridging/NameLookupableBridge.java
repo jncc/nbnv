20409b4429fb6575907ef35af81c4045c701c69f
Chris <cjohn@ceh.ac.uk>
2012-11-12 15:51:15 +0000

    Removing old java code for the imt

--- imt/src/main/java/nbn/common/bridging/NameLookupableBridge.java
+++
@@ -1,9 +0,0 @@
-package nbn.common.bridging;
-
-/**
- *
- * @author Administrator
- */
-public interface NameLookupableBridge<I,O> extends Bridge<I,O> {
-    public Bridge<I,String> getNamedLookupBridge();
-}
