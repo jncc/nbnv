b4289f8f362079a573779acc7b7f52b627bb8a6a
Christopher Johnson <cjohn@ceh.ac.uk>
2012-11-21 08:58:10 +0000

    Validating user bean, ensuring that an email address is specified

--- api.model/src/main/java/uk/org/nbn/nbnv/api/model/User.java
+++ api.model/src/main/java/uk/org/nbn/nbnv/api/model/User.java
@@ -1,7 +1,6 @@
 package uk.org.nbn.nbnv.api.model;
 
 import java.util.Date;
-import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -26,6 +25,7 @@ public class User {
     @NotEmpty
     private String username;
     
+    @NotEmpty
     @Email
     private String email;
     
