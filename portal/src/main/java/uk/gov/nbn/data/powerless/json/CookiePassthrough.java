c8c362a59fe480934e583eddf947cc64c753e775
Chris <cjohn@ceh.ac.uk>
2012-11-22 16:11:39 +0000

    setting correct link for datasets table page moving error pages clearing out some unused imports

--- portal/src/main/java/uk/gov/nbn/data/powerless/json/CookiePassthrough.java
+++ portal/src/main/java/uk/gov/nbn/data/powerless/json/CookiePassthrough.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import javax.net.ssl.SSLEngineResult;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
