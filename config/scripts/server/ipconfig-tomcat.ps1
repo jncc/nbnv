f3a25e69b509f01b6b99986d511ef545c6b1932c
Christopher Johnson <cjohn@ceh.ac.uk>
2013-02-25 13:47:52 +0000

    Requesting domain name when running ipconfig-tomcat

--- config/scripts/server/ipconfig-tomcat.ps1
+++ config/scripts/server/ipconfig-tomcat.ps1
@@ -1,27 +1,18 @@
 ###############################################################################
-# The following hash defines which tomcat connects to which network connection.
-# The Connection names are defined in:
-#		Control Panel\Network and Internet\Network Connections
+# The following powershell script will allow you to update the various tomats
+# to respond on given hostnames addresses. Only the server.xmls will be updated
+# no services will be restarted
 ###############################################################################
-$tomcatAdapters = @{
-	"api"="api.nbn.org.uk Network Connection";
-	"data"="data.nbn.org.uk Network Connection";
-	"gis"="gis.nbn.org.uk Network Connection";
-}
-
-# Get the named adapters. Convert adapter configuration to lookable hash
-$netConfig = @{}; $namedAdapters = @{}
-Get-WmiObject win32_networkadapterconfiguration | % {$netConfig[$_.Index] = $_}
-Get-WmiObject win32_networkadapter | Where-Object {$_.NetConnectionID -ne $null} | % {
-	$namedAdapters += @{$_.NetConnectionID=$netConfig[$_.Index]} 
-}
+$tomcats = @("api","data","gis")
 
 Function Set-Address { 
-	param($adapter); 
-	$input -replace 'address="[^"]*"',('address="{0}"' -f $namedAdapters[$adapter].IPAddress )
+	param($domainname);
+	$ipaddress = [System.Net.Dns]::GetHostAddresses($domainname)[0].IPAddressToString;
+	$input  -replace 'address="[^"]*"',('address="{0}"' -f $ipaddress )
 }
 
-$tomcatAdapters.getEnumerator() | % { 					
-	$path = "D:\Tomcat7\{0}\conf\server.xml" -f $_.key	#Update the server.xml
-	cat $path | Set-Address -adapter $_.value | Set-Content $path
+$tomcats | % {
+	$path = "D:\Tomcat7\{0}\conf\server.xml" -f $_	#Update the server.xml
+	$domainname = Read-Host "Enter the tomcat address for $_"
+	cat $path | Set-Address -domainname $domainname | Set-Content $path
 }
