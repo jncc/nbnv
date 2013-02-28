c89c5a28d11b3a9bd87a58ea6a388e6440f2fde7
Paul Gilbertson <paulbe@ceh.ac.uk>
2013-02-28 10:12:44 +0000

    [NBNV-319] Order Habitat datasets by title

--- api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/HabitatDatasetMapper.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/HabitatDatasetMapper.java
@@ -7,7 +7,7 @@ import uk.org.nbn.nbnv.api.model.HabitatDataset;
 
 public interface HabitatDatasetMapper {
 
-    @Select("SELECT * FROM HabitatDatasetData")
+    @Select("SELECT * FROM HabitatDatasetData ORDER BY title")
     List<HabitatDataset> get();
 
     @Select("SELECT * FROM HabitatDatasetData where datasetKey = #{id}")
