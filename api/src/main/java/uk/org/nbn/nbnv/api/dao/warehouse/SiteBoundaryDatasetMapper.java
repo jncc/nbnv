397426b80359da798a349d36aaeadc79cff4c333
Paul Gilbertson <paulbe@ceh.ac.uk>
2013-03-07 16:40:19 +0000

    [NBNV-342] Order siteboundaries by title

--- api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/SiteBoundaryDatasetMapper.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/SiteBoundaryDatasetMapper.java
@@ -7,7 +7,7 @@ import uk.org.nbn.nbnv.api.model.SiteBoundaryDataset;
 
 public interface SiteBoundaryDatasetMapper {
 
-    @Select("SELECT * FROM SiteBoundaryDatasetData")
+    @Select("SELECT * FROM SiteBoundaryDatasetData ORDER BY title")
     List<SiteBoundaryDataset> get();
     
     @Select("SELECT * FROM SiteBoundaryDatasetData where siteBoundaryCategory = #{id} order by title asc")
