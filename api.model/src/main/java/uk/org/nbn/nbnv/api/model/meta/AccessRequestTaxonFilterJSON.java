ce8e84ed64babece6ae0a1909b9243e15d313d3b
Paul Gilbertson <paulbe@ceh.ac.uk>
2013-04-29 10:57:21 +0100

    Match model to new taxon filter

--- api.model/src/main/java/uk/org/nbn/nbnv/api/model/meta/AccessRequestTaxonFilterJSON.java
+++ api.model/src/main/java/uk/org/nbn/nbnv/api/model/meta/AccessRequestTaxonFilterJSON.java
@@ -10,6 +10,8 @@ package uk.org.nbn.nbnv.api.model.meta;
  */
 public class AccessRequestTaxonFilterJSON extends AccessRequestFilterJSON {
     private String tvk;
+    private String designation;
+    private String output;
 
     /**
      * @return the tvk
@@ -23,6 +25,34 @@ public class AccessRequestTaxonFilterJSON extends AccessRequestFilterJSON {
      */
     public void setTvk(String tvk) {
         this.tvk = tvk;
+    }
+
+    /**
+     * @return the designation
+     */
+    public String getDesignation() {
+        return designation;
+    }
+
+    /**
+     * @param designation the designation to set
+     */
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    /**
+     * @return the output
+     */
+    public String getOutput() {
+        return output;
+    }
+
+    /**
+     * @param output the output to set
+     */
+    public void setOutput(String output) {
+        this.output = output;
     }
     
 }
