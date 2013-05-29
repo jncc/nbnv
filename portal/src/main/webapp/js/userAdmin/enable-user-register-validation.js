a8470e4755ec9b21bff2144b5d676577b6e3d2ee
Matt Debont <matt.debont@devlag.org>
2013-05-29 14:49:09 +0100

    Added a minimum length of 5 to any acceptable password, but added notification that the password is very weak and gives suggestions on how to improve the password strength. This change lives on the registration and user password change pages.
    Added some feedback to the registration form so it pre-validates the data before its sent off on the registration form, with appropriate errors rather than the standard must exist errors

---
+++ portal/src/main/webapp/js/userAdmin/enable-user-register-validation.js
@@ -0,0 +1,77 @@
+(function($) {
+    $(document).ready(function() {
+        $('#password').passField({
+            acceptRate: 1,
+            pattern: 'Abcde12',
+            showToggle: false,
+            showGenerate: false,
+            showWarn: false,
+            localeMsg: {
+                noCharType: "Password should contain {}",
+                passTooShort: "Password should be more than {} characters long"
+            },
+            locale: "en"
+        });
+
+        var container = $('#errors');
+
+        $('#register-form').validate({
+            errorLabelContainer: $('#register-form div.error'),
+            submitHandler: function(form) {
+                // If the phone number is blank then set the text to "No number avail"
+                if ($('#phone').val().trim() === '') {
+                    $('#phone').val('No number avail');
+                }
+                form.submit();
+            },
+            invalidHandler: function() {
+                $('#errors').show();
+                $('#user.errors').hide();
+            },
+            rules: {
+                username: {
+                    required: true
+                },
+                forename: {
+                    required: true
+                },
+                surname: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                password_confirm: {
+                    equalTo: '#password'
+                }
+            },
+            messages: {
+                username: {
+                    required: 'Please enter a username'
+                },
+                forename: {
+                    required: 'Please enter your First Name'
+                },
+                surname: {
+                    required: 'Please enter your Last Name'
+                },
+                email: {
+                    required: 'Please enter a valid email address',
+                    email: 'Please enter a valid email address'
+                },
+                password: {
+                    required: "Please provide a password",
+                    minlength: "Your password must be at least 5 characters long"
+                },
+                password_confirm: {
+                    equalTo: "Please enter the same password as above"
+                }
+            }
+        });
+    });
+})(jQuery);
