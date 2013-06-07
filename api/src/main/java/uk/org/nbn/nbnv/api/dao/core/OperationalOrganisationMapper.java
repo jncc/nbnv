f57fe0b4b7928e050d90027571e9f29c79d7f29f
Matt Debont <matt.debont@devlag.org>
2013-06-07 14:28:15 +0100

    Added organisation metadata edit to the admin page, checkbox currently does not work correctly however

--- api/src/main/java/uk/org/nbn/nbnv/api/dao/core/OperationalOrganisationMapper.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/dao/core/OperationalOrganisationMapper.java
@@ -5,7 +5,9 @@
 package uk.org.nbn.nbnv.api.dao.core;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import uk.org.nbn.nbnv.api.model.Organisation;
 
 /**
@@ -13,13 +15,35 @@ import uk.org.nbn.nbnv.api.model.Organisation;
  * @author paulbe
  */
 public interface OperationalOrganisationMapper {
-    @Select("SELECT CASE WHEN logo IS NULL THEN 0 ELSE 1 END as hasLogo, CASE WHEN logoSmall IS NULL THEN 0 ELSE 1 END as hasSmallLogo, id, name, abbreviation, summary, address, postcode, contactName, contactEmail, website, allowPublicRegistration FROM OrganisationData WHERE id = #{id}")
+    @Select("SELECT CASE WHEN logo IS NULL THEN 0 ELSE 1 END as hasLogo, CASE WHEN logoSmall IS NULL THEN 0 ELSE 1 END as hasSmallLogo, id, name, abbreviation, summary, address, postcode, contactName, contactEmail, website, allowPublicRegistration FROM Organisation WHERE id = #{id}")
     Organisation selectByID(int id);
     
-    @Select("SELECT org.* FROM OrganisationData org INNER JOIN OrganisationMembershipData omd ON omd.organisationID = org.id WHERE omd.userID = #{id}")
+    @Select("SELECT org.* FROM Organisation org INNER JOIN OrganisationMembership omd ON omd.organisationID = org.id WHERE omd.userID = #{id}")
     List<Organisation> selectByUser(int id);
 
-    @Select("SELECT org.* FROM OrganisationData org INNER JOIN OrganisationMembershipData omd ON omd.organisationID = org.id WHERE omd.userID = #{id} AND (omd.role = 'administrator' OR omd.role = 'lead')")
+    @Select("SELECT org.* FROM Organisation org INNER JOIN OrganisationMembership omd ON omd.organisationID = org.id WHERE omd.userID = #{id} AND (omd.role = 'administrator' OR omd.role = 'lead')")
     List<Organisation> selectByAdminUser(int id);
     
+    @Update("UPDATE Organisation SET name = #{name}, abbreviation = #{abbreviation}, "
+            + "summary = #{summary}, address = #{address}, postcode = #{postcode}, "
+            + "phone = #{phone}, website = #{website}, contactName = #{contactName}, "
+            + "contactEmail = #{contactEmail}, allowPublicRegistration = #{allowPublicRegistration} "
+            + "WHERE id = #{id}")
+    int updateOrganisationDetails(
+            @Param("id") int id,
+            @Param("name") String name,
+            @Param("abbreviation") String abbreviation,
+            @Param("summary") String summary,
+            @Param("address") String address,
+            @Param("postcode") String postcode,
+            @Param("phone") String phone,
+            @Param("website") String website,
+            @Param("contactName") String contactName,
+            @Param("contactEmail") String contactEmail,
+            @Param("allowPublicRegistration") int allowPublicRegistration);
+    
+    @Update("UPDATE Organisation SET logo = #{logo}, smallLogo = #{smallLogo}")
+    int updateOrganisationLogo(
+            @Param("logo") byte[] logo,
+            @Param("smallLogo") byte[] smallLogo);
 }
