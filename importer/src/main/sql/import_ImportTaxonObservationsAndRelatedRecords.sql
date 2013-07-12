84997f51241051ca8e551c5d21da6e0bbdd8c583
felix mason <felix.mason@jncc.gov.uk>
2013-07-12 12:43:06 +0100

    More chages to the import sproc to support updates

--- importer/src/main/sql/import_ImportTaxonObservationsAndRelatedRecords.sql
+++ importer/src/main/sql/import_ImportTaxonObservationsAndRelatedRecords.sql
@@ -1,5 +1,7 @@
+USE [NBNCore]
+GO
 
-/****** Object:  StoredProcedure [dbo].[import_DeleteTaxonObservationsAndRelatedRecords]    Script Date: 07/12/2013 09:40:45 ******/
+/****** Object:  StoredProcedure [dbo].[import_ImportTaxonObservationsAndRelatedRecords]    Script Date: 07/10/2013 12:07:44 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -8,55 +10,406 @@ GO
 
 
 
-------------------------------
 
 -- =============================================
--- Author:		Pete
--- Create date: 20121026
--- Description:	Used by the importer for re-importing existing datasets - the way we do this is by updating the
--- Dataset/TaxonDataset records but completely deleting the associated observation records and re-importing.
+-- Author:  Felix Mason
+-- Create date: 31/05/2013
+-- Description: Import data from the import staging
+-- tables into the core database
 -- =============================================
-CREATE PROCEDURE [dbo].[import_DeleteTaxonObservationsAndRelatedRecords] 
-	@datasetKey char(8)
+alter PROCEDURE [dbo].[import_ImportTaxonObservationsAndRelatedRecords]
 AS
-BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+BEGIN TRY
+	BEGIN TRANSACTION
 
-	IF NOT EXISTS(SELECT TOP 1 * FROM dbo.Dataset d WHERE d.[key] = @datasetKey) RETURN;
 
-	DELETE FROM dbo.[TaxonObservationAttribute] WHERE observationID IN (
-		SELECT o.id
-		FROM dbo.[Survey] r 
-		INNER JOIN dbo.[Sample] s ON s.surveyID = r.id 
-		INNER JOIN dbo.[TaxonObservation] o on o.sampleID = s.id
-		WHERE r.datasetKey = @datasetKey
-	);
+		DECLARE @DatasetKey char(8) = (SELECT TOP 1 [Key] FROM dbo.ImportDataset);
+		
+		IF @DatasetKey <> 'NewDst00' AND NOT EXISTS (SELECT TOP 1 ds.[key] FROM Dataset ds WHERE ds.[key] = @DatasetKey)
+		BEGIN
+			DECLARE @ErrorMsg varchar(20) = 'Unknown dataset key: ' + @DatasetKey
+			RAISERROR(@ErrorMsg,16,1)
+		END
+		
+		--DATASET IMPORT
+		--Update Existing dataset
+		UPDATE [dbo].[Dataset]
+		   SET [datasetTypeKey] = ids.[datasetTypeKey]
+			  ,[providerOrganisationKey] = ids.[providerOrganisationKey]
+			  ,[title] = ids.[title]
+			  ,[description] = ids.[description]
+			  ,[providerKey] = ids.[providerKey]
+			  ,[dataCaptureMethod] = ids.[dataCaptureMethod]
+			  ,[purpose] = ids.[purpose]
+			  ,[geographicalCoverage] = ids.[geographicalCoverage]
+			  ,[temporalCoverage] = ids.[temporalCoverage]
+			  ,[dataQuality] = ids.[dataQuality]
+			  ,[additionalInformation] = ids.[additionalInformation]
+			  ,[accessConstraints] = ids.[accessConstraints]
+			  ,[useConstraints] = ids.[useConstraints]
+			  ,[updateFrequencyCode] = ids.[updateFrequencyCode]
+			  ,[dateUploaded] = ids.[dateUploaded]
+			  ,[metadataLastEdited] = ids.[metadataLastEdited]
+		 FROM dbo.Dataset ds
+			INNER JOIN dbo.ImportDataset ids ON ds.[key] = ids.[key];
+			
+		--Insert New Datasets
+		IF EXISTS(SELECT TOP 1 * FROM dbo.ImportDataset ids WHERE [key] = 'NewDst00')
+		BEGIN
+		
+			DECLARE @newKey as TABLE ([key] char(8))
+			
+			INSERT INTO [dbo].[Dataset]
+			   ([key]
+			   ,[datasetTypeKey]
+			   ,[providerOrganisationKey]
+			   ,[title]
+			   ,[description]
+			   ,[providerKey]
+			   ,[dataCaptureMethod]
+			   ,[purpose]
+			   ,[geographicalCoverage]
+			   ,[temporalCoverage]
+			   ,[dataQuality]
+			   ,[additionalInformation]
+			   ,[accessConstraints]
+			   ,[useConstraints]
+			   ,[updateFrequencyCode]
+			   ,[dateUploaded]
+			   ,[metadataLastEdited])
+			   OUTPUT INSERTED.[key] INTO @newKey
+			 SELECT top 1
+				  dbo.fn_GetNewDatasetKey() AS newKey
+				  ,datasetTypeKey
+				  ,providerOrganisationKey
+				  ,title
+				  ,description
+				  ,providerKey
+				  ,dataCaptureMethod
+				  ,purpose
+				  ,geographicalCoverage
+				  ,temporalCoverage
+				  ,dataQuality
+				  ,additionalInformation
+				  ,accessConstraints
+				  ,useConstraints
+				  ,updateFrequencyCode
+				  ,dateUploaded
+				  ,metadataLastEdited 
+			FROM dbo.ImportDataset ids
+			WHERE ids.[key] = 'NewDst00'
+			
+			SET @DatasetKey = (SELECT TOP 1 [key] FROM @newKey)
+		END
+		
+		--Update existing taxon datasets
+		UPDATE [dbo].[TaxonDataset]
+		   SET [publicResolutionID] = its.[publicResolutionID]
+			  ,[allowRecordValidation] = its.allowRecordValidation
+			  ,[publicAttribute] = its.publicAttribute
+		FROM dbo.[TaxonDataset] ts
+			INNER JOIN dbo.ImportTaxonDataset its on ts.datasetKey = its.datasetKey;
+			
+		--Import new taxon datasets
+		INSERT INTO [dbo].[TaxonDataset]
+			   ([datasetKey]
+			   ,[publicResolutionID]
+			   ,[allowRecordValidation]
+			   ,[publicAttribute])
+		SELECT @DatasetKey
+			,ids.publicResolutionID
+			,ids.allowRecordValidation
+			,ids.publicAttribute
+		FROM dbo.ImportTaxonDataset ids
+		WHERE ids.datasetKey = 'NewDst00';
+			
+		
+		--SURVEY IMPORT	
+		--Import new surveys
+		INSERT INTO dbo.Survey
+			(datasetKey
+			,providerKey
+			,title)
+		SELECT
+			@DatasetKey
+			,ims.providerKey
+			,ims.title 
+		FROM dbo.ImportSurvey ims
+			LEFT JOIN dbo.Survey s ON s.datasetKey = @DatasetKey AND ims.providerKey = s.providerKey
+		WHERE s.id IS NULL;
+			
+		--SAMPLE IMPORT
+		--Insert new samples
+		INSERT INTO dbo.Sample
+			(surveyID
+			,providerKey)
+		SELECT 
+			sv.id
+			,ims.providerKey
+		FROM dbo.ImportSample ims
+			INNER JOIN dbo.ImportSurvey imsv ON ims.surveyID = imsv.id
+			INNER JOIN dbo.Survey sv ON sv.datasetKey = @DatasetKey AND imsv.providerKey = sv.providerKey
+			LEFT JOIN dbo.[Sample] s ON s.surveyID = sv.id AND s.providerKey = ims.providerKey
+		WHERE s.id IS NULL;
+		
+		--SITE IMPORT
+		--Import new sites - sites can match on provider key or name (with no key)
+		INSERT INTO dbo.Site
+			(datasetKey
+			,providerKey
+			,name)
+		SELECT @DatasetKey
+			,ims.providerKey
+			,ims.name
+		FROM dbo.ImportSite ims
+			LEFT JOIN dbo.Site s on s.datasetKey = @DatasetKey AND (
+				(ims.providerKey is null and s.providerKey is null and ims.name = s.name)
+				OR  ims.providerKey = s.providerKey)
+		WHERE s.id IS NULL;
+		
+		--RECORDER IMPORT
+		--Insert new recorders and determiners
+		INSERT INTO dbo.Recorder (name)
+		SELECT
+			LTRIM(RTRIM(ir.name))
+		FROM dbo.ImportRecorder ir 
+			LEFT JOIN dbo.Recorder r ON ir.name = r.name
+		WHERE r.id IS NULL;
 	
-	DELETE FROM [TaxonObservationPublic] where taxonObservationID in (
-		SELECT o.id
-		FROM dbo.[Survey] r 
-		INNER JOIN dbo.[Sample] s ON s.surveyID = r.id 
-		INNER JOIN dbo.[TaxonObservation] o on o.sampleID = s.id
-		WHERE r.datasetKey = @datasetKey
-	);
+		--RECORD IMPORT.
+		
+		DECLARE @UpdatedTaxonObservations AS TABLE 
+			(UpdatedTaxonObservationID int);
+		
+		--INSERT INTO @UpdatedTaxonObservations
+		--	(UpdatedTaxonObservationID)
+		--SELECT tob.Id
+		--FROM dbo.TaxonObservation tob
+		--	INNER JOIN dbo.Sample s ON tob.sampleID = s.id
+		--	INNER JOIN dbo.Survey sv ON sv.id = s.surveyID
+		--		AND sv.datasetKey = @DatasetKey
+		--	INNER JOIN dbo.ImportSurvey isv ON sv.providerKey = isv.providerKey
+		--	INNER JOIN dbo.ImportSample ism ON ism.surveyID = isv.id
+		--	INNER JOIN dbo.ImportTaxonObservation itob ON ism.id = itob.sampleID
+		--		AND itob.providerKey = tob.providerKey;
+			
+		
+		--DELETE FROM dbo.TaxonObservationAttribute
+		--WHERE observationID IN 
+		--	(SELECT uto.UpdatedTaxonObservationID FROM @UpdatedTaxonObservations uto);
+			
+		--DELETE FROM dbo.TaxonObservation
+		--WHERE id IN 
+		--	(SELECT uto.UpdatedTaxonObservationID FROM @UpdatedTaxonObservations uto);*-
+				
 
-	DELETE FROM [TaxonObservation] WHERE sampleID IN (
-		SELECT s.id
-		FROM [Survey] r 
-		INNER JOIN [Sample] s ON s.surveyID = r.id
-		WHERE r.datasetKey = @datasetKey
-	);
+		--DECLARE @TaxonObservationMap AS TABLE 
+		--	(InsertedTaxonObservationId int
+		--	,ImportTaxonObservationId int);
+		
+		
+		MERGE [dbo].[TaxonObservation] as tob
+		USING
+			(SELECT sam.id AS [SampleId]
+			  ,itob.[providerKey]
+			  ,itob.[taxonVersionKey]
+			  ,itob.[dateStart]
+			  ,itob.[dateEnd]
+			  ,itob.[dateTypeKey]
+			  ,st.id AS SiteId
+			  ,itob.[FeatureId]
+			  ,itob.[absenceRecord]
+			  ,itob.[sensitiveRecord]
+			  ,rc.id AS [recorderID]
+			  ,dt.id AS  [determinerID]
+			  ,itob.id AS [ImportTaxonObservationId]
+			FROM dbo.ImportTaxonObservation itob
+				INNER JOIN dbo.ImportSample isam ON itob.sampleID = isam.id 
+				INNER JOIN dbo.ImportSurvey isrv ON isam.surveyID = isrv.id
+				INNER JOIN dbo.Survey srv ON srv.providerKey = isrv.providerKey
+					AND srv.datasetKey = @DatasetKey
+				INNER JOIN dbo.[Sample] sam ON sam.surveyID = srv.id 
+					AND sam.providerKey = isam.providerKey
+				LEFT JOIN dbo.ImportSite ist ON itob.siteID = ist.id
+				LEFT JOIN dbo.[Site] st ON ((ist.providerKey = st.providerKey)
+						OR (ist.providerKey IS NULL AND st.providerKey IS NULL AND st.name = ist.name))
+					AND st.datasetKey = @DatasetKey
+				LEFT JOIN dbo.ImportRecorder irc ON itob.recorderID = irc.id
+				LEFT JOIN dbo.Recorder rc ON irc.name = rc.name
+				LEFT JOIN dbo.ImportRecorder idt ON itob.determinerID = idt.id
+				LEFT JOIN dbo.Recorder dt ON idt.name = dt.name) s
+		ON s.SampleId = tob.SampleId AND s.providerKey = tob.providerKey
+		WHEN NOT MATCHED THEN
+			INSERT  
+			([sampleID]
+		   ,[providerKey]
+		   ,[taxonVersionKey]
+		   ,[dateStart]
+		   ,[dateEnd]
+		   ,[dateTypeKey]
+		   ,[siteID]
+		   ,[featureID]
+		   ,[absenceRecord]
+		   ,[sensitiveRecord]
+		   ,[recorderID]
+		   ,[determinerID])
+		   VALUES
+		   (s.SampleId
+		   ,s.[providerKey]
+		   ,s.[taxonVersionKey]
+		   ,s.[dateStart]
+		   ,s.[dateEnd]
+		   ,s.[dateTypeKey]
+		   ,s.[siteID]
+		   ,s.[FeatureID]
+		   ,s.[absenceRecord]
+		   ,s.[sensitiveRecord]
+		   ,s.[recorderID]
+		   ,s.[determinerID])
+		 WHEN MATCHED THEN
+			UPDATE
+		   SET [taxonVersionKey] = s.taxonVersionKey
+			  ,[dateStart] = s.dateStart 
+			  ,[dateEnd] = s.dateEnd 
+			  ,[dateTypeKey] = s.dateTypeKey
+			  ,[siteID] = s.siteID 
+			  ,[featureID] = s.featureID 
+			  ,[absenceRecord] = s.absenceRecord 
+			  ,[sensitiveRecord] = s.sensitiveRecord 
+			  ,[recorderID] = s.recorderID 
+			  ,[determinerID] = s.determinerID;
+		 
+		
+		MERGE [dbo].[TaxonObservationPublic] AS tobp
+		USING
+				(SELECT tob.id AS taxonObservationID
+					,st.id AS siteId
+					,itobp.featureID AS featureID
+					,rr.id AS recorderID
+					,rd.id AS determinerID
+			FROM dbo.ImportTaxonObservationPublic itobp
+				INNER JOIN dbo.ImportTaxonObservation itob ON itobp.taxonObservationID = itob.id
+				INNER JOIN dbo.ImportSample isam ON itob.sampleID = isam.id 
+				INNER JOIN dbo.ImportSurvey isrv ON isam.surveyID = isrv.id
+				INNER JOIN dbo.Survey srv ON srv.providerKey = isrv.providerKey
+					AND srv.datasetKey = @DatasetKey
+				INNER JOIN dbo.[Sample] sam ON sam.surveyID = srv.id 
+					AND sam.providerKey = isam.providerKey
+				INNER JOIN dbo.TaxonObservation tob ON tob.sampleID = sam.id 
+					AND tob.providerKey = itob.providerKey
+				LEFT JOIN dbo.ImportSite ist ON itobp.siteID = ist.id
+				LEFT JOIN dbo.[Site] st ON ((ist.providerKey = st.providerKey)
+						OR (ist.providerKey IS NULL AND st.providerKey IS NULL AND st.name = ist.name))
+					AND st.datasetKey = @DatasetKey
+				LEFT JOIN dbo.ImportRecorder irr ON itobp.recorderID = irr.id
+				LEFT JOIN dbo.Recorder rr ON irr.name = rr.name
+				LEFT JOIN dbo.ImportRecorder ird ON itobp.determinerId = ird.id
+				LEFT JOIN dbo.Recorder rd ON ird.name = rd.name ) s
+		ON s.taxonObservationID = tobp.taxonObservationID
+		WHEN NOT MATCHED THEN
+			INSERT 
+		   ([taxonObservationID]
+		   ,[siteID]
+		   ,[featureID]
+		   ,[recorderID]
+		   ,[determinerID])
+			VALUES
+			(s.taxonObservationID
+			,s.siteID
+			,s.featureID
+			,s.recorderID
+			,s.determinerID)
+		WHEN MATCHED THEN
+			UPDATE 
+			   SET [siteID] = s.siteID 
+				  ,[featureID] = s.featureID 
+				  ,[recorderID] = s.recorderID 
+				  ,[determinerID] = s.determinerID;
+			         
+         --INSERT ATTRIBUTES
+		INSERT INTO dbo.Attribute
+		   ([label]
+		   ,[description]
+		   ,[storageLevelID]
+		   ,[storageTypeID]
+		   ,[gatewayAttributeID])
+		SELECT ia.[label]
+			  ,ia.[description]
+			  ,ia.[storageLevelID]
+			  ,ia.[storageTypeID]
+			  ,ia.[gatewayAttributeID]
+		FROM [dbo].[ImportAttribute] ia
+			LEFT JOIN dbo.Attribute a ON ia.label = a.label
+				AND ia.[storageLevelID] = a.[storageLevelID]
+				AND ia.[storageTypeID] = a.[storageTypeID]
+		WHERE a.id IS NULL
+			
+		MERGE [dbo].[TaxonObservationAttribute] toa
+		USING
+			(SELECT tob.id AS observationID
+				,a.id AS attributeID
+				,itoba.decimalValue
+				,itoba.enumValue
+				,itoba.textValue
+			FROM dbo.ImportAttribute ia
+				INNER JOIN dbo.ImportTaxonObservationAttribute itoba ON ia.id = itoba.attributeID
+				INNER JOIN dbo.ImportTaxonObservation itob ON itoba.observationID = itob.id
+				INNER JOIN dbo.ImportSample isam ON itob.sampleID = isam.id 
+				INNER JOIN dbo.ImportSurvey isrv ON isam.surveyID = isrv.id
+				INNER JOIN dbo.Survey srv ON srv.providerKey = isrv.providerKey
+					AND srv.datasetKey = @DatasetKey
+				INNER JOIN dbo.[Sample] sam ON sam.surveyID = srv.id 
+					AND sam.providerKey = isam.providerKey
+				INNER JOIN dbo.TaxonObservation tob ON tob.sampleID = sam.id 
+					AND tob.providerKey = itob.providerKey
+				INNER JOIN dbo.Attribute a ON ia.label = a.label
+					AND ia.[storageLevelID] = a.[storageLevelID]
+					AND ia.[storageTypeID] = a.[storageTypeID]) s
+		ON s.observationID = toa.observationID AND s.attributeID = toa.attributeID
+		WHEN NOT MATCHED THEN
+			INSERT
+           ([observationID]
+           ,[attributeID]
+           ,[decimalValue]
+           ,[enumValue]
+           ,[textValue])
+			 VALUES
+		   (s.observationID
+		   ,s.attributeID
+		   ,s.decimalValue
+		   ,s.enumValue
+		   ,s.textValue)
+		WHEN MATCHED THEN
+			UPDATE 
+			   SET [observationID] = s.observationID
+				  ,[attributeID] = s.attributeID
+				  ,[decimalValue] = s.decimalValue
+				  ,[enumValue] = s.enumValue
+				  ,[textValue] = s.textValue;
 
-	DELETE FROM [Sample] WHERE surveyID IN (
-		SELECT id FROM [Survey] WHERE datasetKey = @datasetKey
-	);
+		 
+	COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION;
 	
-	DELETE FROM [Survey] WHERE datasetKey = @datasetKey;
+    --Report errors back to the client
+	DECLARE @ErrorMessage NVARCHAR(4000);
+	DECLARE @ErrorSeverity INT;
+	DECLARE @ErrorState INT;
 
-	DELETE FROM [Site] WHERE datasetKey = @datasetKey;
-END
+	SELECT 
+		@ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+	
+	RAISERROR (@ErrorMessage, -- Message text.
+			@ErrorSeverity, -- Severity.
+			@ErrorState -- State
+			);
+				
+	RETURN;
+END CATCH
 
 
 
