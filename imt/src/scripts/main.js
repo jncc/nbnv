a23d1de0fbcfe1285dee1d6601e4c44dd01c487d
Christopher Johnson <cjohn@ceh.ac.uk>
2013-07-17 08:57:27 +0100

    Converting tabs to spaces

--- imt/src/scripts/main.js
+++ imt/src/scripts/main.js
@@ -1,34 +1,34 @@
 require.config({
-	stubModules: ['cs', 'hbs'],
-	shim: {
+  stubModules: ['cs', 'hbs'],
+  shim: {
     'handlebars': { exports: 'Handlebars' },
-		'openlayers': { exports: 'OpenLayers', deps: ['proj4js']},
-		'jquery-ui': { exports: '$', deps: ['jquery']},
-		'jquery-md5': { exports: '$', deps: ['jquery']},
-		'DataTables' : { exports: '$', deps: ['jquery']}
+    'openlayers': { exports: 'OpenLayers', deps: ['proj4js']},
+    'jquery-ui': { exports: '$', deps: ['jquery']},
+    'jquery-md5': { exports: '$', deps: ['jquery']},
+    'DataTables' : { exports: '$', deps: ['jquery']}
   },
-	paths: {
-		'cs' : '../vendor/require-cs/cs',
-		'coffee-script': '../vendor/coffee-script/extras/coffee-script',
-		'hbs' : '../vendor/requirejs-hbs/hbs',
-		'handlebars' : '../vendor/handlebars/Handlebars',
-		'text' : '../vendor/requirejs-text/text',
-		'DataTables': '../vendor/datatables/media/js/jquery.dataTables',
-		'jquery' : '../vendor/jquery/jquery',
-		'jquery-ui': '../vendor/jquery-ui/ui/jquery-ui',
-		'jquery-md5': '../vendor/jquery-md5/jquery.md5',
-		'jquery-colorpicker': '../vendor/jquery-colorpicker/js/colorpicker',
-		'underscore': '../vendor/underscore-amd/underscore',
-		'backbone': '../vendor/backbone-amd/backbone',
-		'openlayers': '../vendor/openlayers-js/index',
-		'proj4js' : '../vendor/proj4js/lib/proj4js-combined',
-		'select2' : '../vendor/select2/select2'
-	}
+  paths: {
+    'cs' : '../vendor/require-cs/cs',
+    'coffee-script': '../vendor/coffee-script/extras/coffee-script',
+    'hbs' : '../vendor/requirejs-hbs/hbs',
+    'handlebars' : '../vendor/handlebars/Handlebars',
+    'text' : '../vendor/requirejs-text/text',
+    'DataTables': '../vendor/datatables/media/js/jquery.dataTables',
+    'jquery' : '../vendor/jquery/jquery',
+    'jquery-ui': '../vendor/jquery-ui/ui/jquery-ui',
+    'jquery-md5': '../vendor/jquery-md5/jquery.md5',
+    'jquery-colorpicker': '../vendor/jquery-colorpicker/js/colorpicker',
+    'underscore': '../vendor/underscore-amd/underscore',
+    'backbone': '../vendor/backbone-amd/backbone',
+    'openlayers': '../vendor/openlayers-js/index',
+    'proj4js' : '../vendor/proj4js/lib/proj4js-combined',
+    'select2' : '../vendor/select2/select2'
+  }
 });
 
 require(['backbone', 'cs!models/App', 'cs!views/AppView', 'cs!routers/GetURLRouter'], function(Backbone, App, AppView, GetURLRouter) {
-	app = new App();
+  app = new App();
   router = new GetURLRouter({model: app});
-	view = new AppView({model : app});
-	router.navigate(location.search);
+  view = new AppView({model : app});
+  router.navigate(location.search);
 });
