7d9e1f6091f198ec50e7fda741992e21332c0f32
Matt Debont <matt.debont@devlag.org>
2013-08-09 10:56:40 +0100

    [NBNV-513] Fixed error case, have an issue with last updated date not being set correctly

--- portal/src/main/webapp/js/admin/metadata.js
+++ portal/src/main/webapp/js/admin/metadata.js
@@ -1,12 +1,19 @@
 (function($){
     function submit() {
+        $('#nbn-form-submit').attr('disabled','disabled');
+        $("#nbn-waiting-note").show()
         $.ajax({ 
             type: 'POST',
             url: $('#nbn-metadata-update').attr('url'),
-            data: $('#nbn-metadata-update').serialize()
+            data: $('#nbn-metadata-update').serialize(),
+            success: function(result) {
+                window.location = "/User/Admin";
+            },
+            error: function(result) {
+                $("#nbn-waiting-note").hide()
+                alert("An unknown error occured while submitting these changes, please try again later");
+            }
         });
-        
-        window.location = "/User/Admin";
         
         return false;
     }
