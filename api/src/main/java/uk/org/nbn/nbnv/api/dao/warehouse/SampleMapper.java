6231039b42e724606b4ed5f21b4432e6ddc97911
felix mason <felix.mason@jncc.gov.uk>
2013-08-14 15:01:59 +0100

    Sample mapper fixes

--- api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/SampleMapper.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/SampleMapper.java
@@ -1,6 +1,7 @@
 package uk.org.nbn.nbnv.api.dao.warehouse;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import uk.org.nbn.nbnv.api.model.Sample;
 
@@ -11,9 +12,9 @@ public interface SampleMapper {
     List<Sample> selectSamplesBySurveyID(int surveyID);
     
     @Select("SELECT smd.* FROM SampleData smd INNER JOIN SurveyData svd ON svd.id = smd.surveyId WHERE svd.providerKey = #{providerKey} AND svd.datasetKey = #{datasetKey}")
-    List<Sample> selectSamplesBySurveyProviderKey(String datasetKey, String providerKey);
+    List<Sample> selectSamplesBySurveyProviderKey(@Param("datasetKey") String datasetKey, @Param("providerKey") String providerKey);
     
-    @Select("SELECT smd.* FROM SampleData smd INNER JOIN SurveyData svd ON svd.id = smd.surveyId WHERE svd.providerKey = #{providerKey} AND svd.datasetKey = #{datasetKey} AND smd.providerKey = #{sampleProviderKey}")
-    Sample selectSampleBySampleProviderKey(String datasetKey, String surveyProviderKey, String sampleProviderKey);
+    @Select("SELECT smd.* FROM SampleData smd INNER JOIN SurveyData svd ON svd.id = smd.surveyId WHERE svd.providerKey = #{surveyKey} AND svd.datasetKey = #{datasetKey} AND smd.providerKey = #{sampleKey}")
+    Sample selectSampleBySampleProviderKey(@Param("datasetKey") String datasetKey, @Param("surveyKey") String surveyKey, @Param("sampleKey") String sampleKey);
 
 }
