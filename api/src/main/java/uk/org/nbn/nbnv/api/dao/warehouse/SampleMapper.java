f8d17c5ef2e3c3b120be38acc4f861a87ec6d12a
felix mason <felix.mason@jncc.gov.uk>
2013-08-15 15:41:01 +0100

    Fixed sample output

--- api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/SampleMapper.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/SampleMapper.java
@@ -2,6 +2,8 @@ package uk.org.nbn.nbnv.api.dao.warehouse;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import uk.org.nbn.nbnv.api.model.Sample;
 
@@ -12,9 +14,23 @@ public interface SampleMapper {
     List<Sample> selectSamplesBySurveyID(int surveyID);
     
     @Select("SELECT smd.* FROM SampleData smd INNER JOIN SurveyData svd ON svd.id = smd.surveyId WHERE svd.providerKey = #{providerKey} AND svd.datasetKey = #{datasetKey}")
+    @Results(value = {
+        @Result(property="sampleID", column="id", javaType=Integer.class),
+        @Result(property="sampleKey", column="providerKey", javaType=String.class),
+        @Result(property="description", column="description", javaType=String.class),
+        @Result(property="geographicalCoverage", column="geographicalCoverage", javaType=String.class),
+        @Result(property="temporalCoverage", column="temporalCoverage", javaType=String.class),
+    })
     List<Sample> selectSamplesBySurveyProviderKey(@Param("datasetKey") String datasetKey, @Param("providerKey") String providerKey);
     
     @Select("SELECT smd.* FROM SampleData smd INNER JOIN SurveyData svd ON svd.id = smd.surveyId WHERE svd.providerKey = #{surveyKey} AND svd.datasetKey = #{datasetKey} AND smd.providerKey = #{sampleKey}")
+    @Results(value = {
+        @Result(property="sampleID", column="id", javaType=Integer.class),
+        @Result(property="sampleKey", column="providerKey", javaType=String.class),
+        @Result(property="description", column="description", javaType=String.class),
+        @Result(property="geographicalCoverage", column="geographicalCoverage", javaType=String.class),
+        @Result(property="temporalCoverage", column="temporalCoverage", javaType=String.class),
+    })
     Sample selectSampleBySampleProviderKey(@Param("datasetKey") String datasetKey, @Param("surveyKey") String surveyKey, @Param("sampleKey") String sampleKey);
 
 }
