953143ac170460f17084356e3ed344a107329171
Paul Gilbertson <paulbe@ceh.ac.uk>
2013-11-08 13:16:52 +0000

    Grant window accounting bug

--- portal/src/main/webapp/js/admin/access/requestGrantDialog.js
+++ portal/src/main/webapp/js/admin/access/requestGrantDialog.js
@@ -92,7 +92,16 @@ nbn.nbnv.ui.dialog.requestGrantDialog = function() {
         }
         
         if (!json.year.all) { filter.startYear = json.year.startYear; filter.endYear = json.year.endYear; }
-        if (!json.spatial.all) { filter.featureID = json.spatial.feature; filter.spatialRelationship = json.spatial.matchType; }
+        if (!json.spatial.all) { 
+            filter.spatialRelationship = json.spatial.matchType; 
+            
+            if (json.spatial.feature) {
+                filter.featureID = json.spatial.feature; 
+            } else if (json.spatial.gridRef) {
+                filter.gridRef = json.spatial.gridRef;
+            }
+        }
+
         if (json.sensitive == 'sans') { filter.sensitive = 'true'; }
         filter.datasetKey = dataset;
 
