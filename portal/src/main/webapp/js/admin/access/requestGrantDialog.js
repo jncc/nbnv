e3895aab2a8b683e1891e7a6e6621e304d3139a7
Matt Debont <matt.debont@devlag.org>
2013-11-11 13:19:46 +0000

    Grey out grant access button until the record count has come back, also display error message and keep button greyed out if no records would be granted (shouldn't happen)

--- portal/src/main/webapp/js/admin/access/requestGrantDialog.js
+++ portal/src/main/webapp/js/admin/access/requestGrantDialog.js
@@ -14,6 +14,7 @@ nbn.nbnv.ui.dialog.requestGrantDialog = function() {
         var _me = this;
         
         this.div = $('<div>')
+            .attr('id', 'grantDialogDiv')
             .append($('<p>')
                 .append('Granting a request gives access to the full version of the requested records to the user or organisation. It will also dispatch an email informing them of the granted access.')
             ).append($('<p>')
@@ -70,13 +71,15 @@ nbn.nbnv.ui.dialog.requestGrantDialog = function() {
     };
     
     this.show = function(id, json, dataset, datasetEndpoint) {
+        $('.ui-dialog-buttonpane button:contains("Grant Request")').button('disable');
+        
         this.requestID = id;
         this.timeLimit = new nbn.nbnv.ui.timeLimit(json);
         $('#granttimelimit').html('');
         $('#granttimelimit').append(this.timeLimit._renderPanel());
         var filter = {};
         $('#granteffect').html('');
-        $('#granteffect').text('Loading request record coverage');
+        $('#granteffect').empty().append($('<img>').attr('src', '/img/ajax-loader.gif')).append('  Loading request record coverage');
         $('#grantanonwarn').hide();
         
         if ('organisationID' in json.reason && json.reason.organisationID != -1) { this.orgReq = true; } else { this.orgReq = false; }
@@ -110,11 +113,22 @@ nbn.nbnv.ui.dialog.requestGrantDialog = function() {
             data: filter,
             success: function(datasets) { 
                 $('#granteffect').html('');
-                $('#granteffect').append('This request covers ' + datasets[0].querySpecificObservationCount + ' of ' + datasets[0].taxonDataset.recordCount + ' records in the dataset, ' + datasets[0].querySpecificSensitiveObservationCount + ' are sensitive records');
                 
-                if (!json.spatial.all && datasets[0].querySpecificObservationCount == datasets[0].querySpecificSensitiveObservationCount) {
-                    $('#grantanonwarn').show();
-                } 
+                if (datasets.length > 0) {
+                    $('#granteffect').append('This request covers ' + datasets[0].querySpecificObservationCount + ' of ' + datasets[0].taxonDataset.recordCount + ' records in the dataset, ' + datasets[0].querySpecificSensitiveObservationCount + ' are sensitive records');
+                    
+                    if (!json.spatial.all && datasets[0].querySpecificObservationCount == datasets[0].querySpecificSensitiveObservationCount) {
+                        $('#grantanonwarn').show();
+                    } 
+
+                    $('.ui-dialog-buttonpane button:contains("Grant Request")').button('enable');
+                } else {
+                    $('#granteffect').html('<p style="color:red">No records would be granted by this request, we recommend you close this request</p>');
+                }
+            },
+            error: function(retVal) {
+                $('#granteffect').html('');
+                $('#granteffect').append('Error while getting request statistics, if this error continues to occur please contact us');
             }
         });
 
