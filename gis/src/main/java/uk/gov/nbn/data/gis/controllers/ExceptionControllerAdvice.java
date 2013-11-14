d05ccacc36ca5a0c8d0a34295793c0b7c098add9
Christopher Johnson <cjohn@ceh.ac.uk>
2013-11-14 17:30:43 +0000

    handling illegalArgumentExceptions

--- gis/src/main/java/uk/gov/nbn/data/gis/controllers/ExceptionControllerAdvice.java
+++ gis/src/main/java/uk/gov/nbn/data/gis/controllers/ExceptionControllerAdvice.java
@@ -4,6 +4,7 @@ import org.hibernate.validator.method.MethodConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,5 +19,12 @@ public class ExceptionControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ModelAndView handleMethodConstraintViolationException(MethodConstraintViolationException exception) {
         return new ModelAndView("invalid-request", "exception", exception);
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleMethodConstraintViolationException(IllegalArgumentException ex) {
+        return ex.getMessage();
     }
 }
