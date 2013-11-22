12c44b2b916dc614efce332f924857d36a409c3a
Christopher Johnson <cjohn@ceh.ac.uk>
2013-11-22 14:04:06 +0000

    NBNV-784 Whitelisting localhost, bars and nbn sites

--- gis/src/main/java/uk/gov/nbn/data/gis/maps/OSModernMap.java
+++ gis/src/main/java/uk/gov/nbn/data/gis/maps/OSModernMap.java
@@ -1,11 +1,14 @@
 package uk.gov.nbn.data.gis.maps;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,9 +19,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class OSModernMap { 
     private Map<String, String> propertiesHash;
+    private AntPathMatcher matcher;
     
     @Autowired
     public OSModernMap(Properties properties) {
+        matcher = new AntPathMatcher();
         propertiesHash = new HashMap<String, String>();
         for(String key: properties.stringPropertyNames()) {
             propertiesHash.put(key, properties.getProperty(key));
@@ -26,11 +31,43 @@ public class OSModernMap {
     }
     
     @RequestMapping("OS-Modern")
-    public ModelAndView getOSMapModel(HttpServletRequest request) { 
-        String referrer = request.getHeader("referer");
-        if(referrer ==null || !referrer.startsWith(propertiesHash.get("ordnanceSurveyAllowedReferrerPrefix"))) {
+    public ModelAndView getOSMapModel(HttpServletRequest request) {
+        if(!isValidRequest(request)) {
             throw new IllegalArgumentException("The ordnance survey map is restricted to use within the gateway");
         }
         return new ModelAndView("OS-Modern.map", propertiesHash);
+    }
+    
+    public boolean isValidRequest(HttpServletRequest request) {        
+        try {
+            return isValidRequest(request.getHeader("referer"), request.getServerName());
+        } catch (MalformedURLException ex) {
+            return false;
+        } 
+    }
+    
+    public boolean isValidRequest(String referrer, String serverName) throws MalformedURLException {
+        if("localhost".equalsIgnoreCase(serverName)) {
+            return true; //If the server is runnint locally, allow requests
+        }
+        else if(referrer != null) { 
+            //if the referer is present check if valid for nbn request
+            //or valid bars request
+            String referrerHost = new URL(referrer).getHost();
+            return matcher.match("*.nbn.org.uk", referrerHost) || 
+                        isValidBarsRequest(referrerHost, serverName);
+        }
+        else {
+            return false;
+        }
+    }
+    
+    //Bars requests must be sent to bars-gis.nbn.org.uk with referers 
+    //  *.ukbars.org.uk
+    //  ukbars.defra.gov.uk
+    private boolean isValidBarsRequest(String referrerHost, String requestPath) {
+        return "bars-gis.nbn.org.uk".equalsIgnoreCase(requestPath) && (
+                matcher.match("*.ukbars.org.uk", referrerHost) || 
+                "ukbars.defra.gov.uk".equalsIgnoreCase(referrerHost));
     }
 }
---
+++ gis/src/test/java/uk/gov/nbn/data/gis/maps/OSModernMapTest.java
@@ -0,0 +1,165 @@
+package uk.gov.nbn.data.gis.maps;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Properties;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Christopher Johnson
+ */
+public class OSModernMapTest {
+    private OSModernMap mapService;
+    
+    @Before
+    public void createOSModernMap() throws IOException {
+        Properties properties = new Properties();
+        properties.load(getClass().getClassLoader().getResourceAsStream("gis.properties"));
+        mapService = new OSModernMap(properties);
+    }
+    
+    @Test
+    public void checkThatBarsRequestOverSentFromSubDomainIsValid() throws MalformedURLException {
+        //Given
+        String referrer = "http://something.ukbars.org.uk";
+        String hostname = "bars-gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        assertTrue("Expected request to be valid", validRequest);
+    }
+    
+    @Test
+    public void checkThatBarsRequestFromDefraIsValid() throws MalformedURLException {
+        //Given
+        String referrer = "http://ukbars.defra.gov.uk";
+        String hostname = "bars-gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        assertTrue("Expected request to be valid", validRequest);
+    }
+    
+    @Test
+    public void checkBarsDefraRequestToNoneBarsEndPoint() throws MalformedURLException {
+        //Given
+        String referrer = "http://ukbars.defra.gov.uk";
+        String hostname = "gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        assertFalse("Expected request to be invalid", validRequest);
+    }
+    
+    @Test
+    public void checkBarsSubdomainRequestToNoneBarsEndPoint() throws MalformedURLException {
+        //Given
+        String referrer = "http://something.ukbars.org.uk";
+        String hostname = "gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        assertFalse("Expected request to be invalid", validRequest);
+    }
+    
+    @Test
+    public void checkThatTheMapWorksOnTheLocalhost() throws MalformedURLException {
+        //Given
+        String hostname = "localhost";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(null, hostname);
+        
+        //Then
+        assertTrue("Expected request to be valid", validRequest);
+    }
+    
+    @Test
+    public void checkThatTheMapWorksFromTheInteractiveMapTool() throws MalformedURLException {
+        //Given
+        String referrer = "http://data.nbn.org.uk/imt";
+        String hostname = "gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        assertTrue("Expected request to be valid", validRequest);
+    }
+    
+    @Test
+    public void checkThatTheMapCantBeHotLinkedFromGoogle() throws MalformedURLException {
+        //Given
+        String referrer = "http://www.google.com";
+        String hostname = "gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        assertFalse("Expected request to be invalid", validRequest);
+    }
+    
+    @Test
+    public void checkThatTheMapDoesntRespondIfRefererAndHostNameArntProvided() throws MalformedURLException {
+        //Given
+        //Nothing
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(null, null);
+        
+        //Then
+        assertFalse("Expected request to be invalid", validRequest);
+    }
+    
+    @Test(expected=MalformedURLException.class)
+    public void checkThatInvalidRefererIsInvalid() throws MalformedURLException {
+        //Given
+        String referrer = "Giberjabber";
+        String hostname = "gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        fail("Expected request to be invalid");
+    }
+    
+    @Test(expected=MalformedURLException.class)
+    public void checkThatEmptyReferrerIsInvalid() throws MalformedURLException {
+        //Given
+        String referrer = "";
+        String hostname = "gis.nbn.org.uk";
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        fail("Expected request to be invalid");
+    }
+    
+    @Test
+    public void checkThatEmptyHostnameIsInvalid() throws MalformedURLException {
+        //Given
+        String referrer = "http://web.site.com";
+        String hostname = null;
+        
+        //When
+        boolean validRequest = mapService.isValidRequest(referrer, hostname);
+        
+        //Then
+        assertFalse("Expected result to be invalid", validRequest);
+        //Then
+    }
+}
