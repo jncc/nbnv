07ec47d13b170592399ccae4daa1258d9e31870c
Matt Debont <matt.debont@devlag.org>
2013-11-22 11:22:58 +0000

    NBNV-781 - Rewrote the recording entity and taxon name checks to work with the new table design

--- crud/src/main/java/uk/org/nbn/nbnv/jpa/nbncore/RecordingEntity.java
+++ crud/src/main/java/uk/org/nbn/nbnv/jpa/nbncore/RecordingEntity.java
@@ -27,50 +27,120 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "RecordingEntity.findAll", query = "SELECT r FROM RecordingEntity r"),
-    @NamedQuery(name = "RecordingEntity.findByRecordedName", query = "SELECT r FROM RecordingEntity r WHERE r.recordedName = :recordedName"),
-    @NamedQuery(name = "RecordingEntity.findByDangerous", query = "SELECT r FROM RecordingEntity r WHERE r.dangerous = :dangerous")})
+    @NamedQuery(name = "RecordingEntity.findByRecordedName", query = "SELECT r FROM RecordingEntity r WHERE r.name = :name"),
+    @NamedQuery(name = "RecordingEntity.findByDangerous", query = "SELECT r FROM RecordingEntity r WHERE r.dangerousName = :dangerousName")})
 public class RecordingEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 255)
-    @Column(name = "recordedName")
-    private String recordedName;
+    @Column(name = "taxonVersionKey")
+    private String taxonVersionKey;
     @Basic(optional = false)
     @NotNull
-    @Column(name = "dangerous")
-    private boolean dangerous;
-    @JoinColumn(name = "taxonVersionKey", referencedColumnName = "taxonVersionKey")
+    @Size(min = 1, max = 255)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "pExtendedName")
+    private String pExtendedName;    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "authority")
+    private String authority;    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "taxonGroup")
+    private String taxonGroup;   
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "taxonRank")
+    private String taxonRank; 
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dangerousName")
+    private String dangerousName;
+    @JoinColumn(name = "pTaxonVersionKey", referencedColumnName = "taxonVersionKey")
     @ManyToOne(optional = false)
-    private Taxon taxon;
-
+    private Taxon taxon; 
+    
     public RecordingEntity() {
     }
 
-    public RecordingEntity(String recordedName) {
-        this.recordedName = recordedName;
+    public RecordingEntity(String name) {
+        this.name = name;
     }
 
-    public RecordingEntity(String recordedName, boolean dangerous) {
-        this.recordedName = recordedName;
-        this.dangerous = dangerous;
+    public RecordingEntity(String name, String dangerousName) {
+        this.name = name;
+        this.dangerousName = dangerousName;
+    }
+    
+    public RecordingEntity(String taxonVersionKey, String name, String pExtendedName, String authority, String taxonGroup, String taxonRank, String dangerousName) {
+        this.taxonVersionKey = taxonVersionKey;
+        this.name = name;
+        this.pExtendedName = pExtendedName;
+        this.authority = authority;
+        this.taxonGroup = taxonGroup;
+        this.taxonRank = taxonRank;
+        this.dangerousName = dangerousName;
+    }    
+
+    public String getTaxonVersionKey() {
+        return taxonVersionKey;
     }
 
-    public String getRecordedName() {
-        return recordedName;
+    public void setTaxonVersionKey(String taxonVersionKey) {
+        this.taxonVersionKey = taxonVersionKey;
     }
 
-    public void setRecordedName(String recordedName) {
-        this.recordedName = recordedName;
+    public String getName() {
+        return name;
     }
 
-    public boolean getDangerous() {
-        return dangerous;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setDangerous(boolean dangerous) {
-        this.dangerous = dangerous;
+    public String getpExtendedName() {
+        return pExtendedName;
+    }
+
+    public void setpExtendedName(String pExtendedName) {
+        this.pExtendedName = pExtendedName;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public String getTaxonGroup() {
+        return taxonGroup;
+    }
+
+    public void setTaxonGroup(String taxonGroup) {
+        this.taxonGroup = taxonGroup;
+    }
+
+    public String getTaxonRank() {
+        return taxonRank;
+    }
+
+    public void setTaxonRank(String taxonRank) {
+        this.taxonRank = taxonRank;
+    }
+
+    public String getDangerousName() {
+        return dangerousName;
+    }
+
+    public void setDangerousName(String dangerousName) {
+        this.dangerousName = dangerousName;
     }
 
     public Taxon getTaxon() {
@@ -84,7 +154,7 @@ public class RecordingEntity implements Serializable {
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (recordedName != null ? recordedName.hashCode() : 0);
+        hash += (name != null ? name.hashCode() : 0);
         return hash;
     }
 
@@ -95,7 +165,7 @@ public class RecordingEntity implements Serializable {
             return false;
         }
         RecordingEntity other = (RecordingEntity) object;
-        if ((this.recordedName == null && other.recordedName != null) || (this.recordedName != null && !this.recordedName.equals(other.recordedName))) {
+        if ((this.name == null && other.name != null) || (this.name != null && !this.name.equals(other.name))) {
             return false;
         }
         return true;
@@ -103,7 +173,7 @@ public class RecordingEntity implements Serializable {
 
     @Override
     public String toString() {
-        return "uk.org.nbn.nbnv.jpa.nbncore.RecordingEntity[ recordedName=" + recordedName + " ]";
+        return "uk.org.nbn.nbnv.jpa.nbncore.RecordingEntity[ recordedName=" + name + " ]";
     }
     
 }
