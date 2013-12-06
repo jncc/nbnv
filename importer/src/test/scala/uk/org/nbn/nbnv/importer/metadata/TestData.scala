566367902dec88cfadb2e9fbb81bce6d8fc92cdf
felix mason <felix.mason@jncc.gov.uk>
2013-12-06 10:39:39 +0000

    Implemented importerType metadata value

--- importer/src/test/scala/uk/org/nbn/nbnv/importer/metadata/TestData.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/metadata/TestData.scala
@@ -56,6 +56,9 @@ object TestData {
       <additionalInfo>
         <para>recordAttributesArePublic: false</para>
       </additionalInfo>
+      <additionalInfo>
+        <para>importType: new</para>
+      </additionalInfo>
     </dataset>
   </eml>
 }
