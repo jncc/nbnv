d7d64b35e31b73254c4156ae450a962dfa63a6ff
felix mason <felix.mason@jncc.gov.uk>
2013-12-17 17:58:27 +0000

    #nbnv-857 - finally got the g*d d*mn zip thing to work YAY

---
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/archive/ArchiveManager.scala
@@ -0,0 +1,15 @@
+package uk.org.nbn.nbnv.importer.archive
+
+import com.google.inject.Inject
+import uk.org.nbn.nbnv.importer.Options
+
+class ArchiveManager @Inject()(options: Options, zipFileManager: ZipFileManager) {
+  //Open zip file
+  //Extract meta data file
+  //Read mapping
+  //Extract data file
+  //if has headers skip line on
+  //read line into raw record using mapping
+  //cl
+
+}
