a274dd65d42f785a5c0ee1d23a70dfca3ada9555
felix mason <felix.mason@jncc.gov.uk>
2013-12-18 10:01:44 +0000

    #nbnv-857 get output of zip as map

--- importer/src/test/scala/uk/org/nbn/nbnv/importer/archive/ZipFileManagerSuite.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/archive/ZipFileManagerSuite.scala
@@ -5,19 +5,34 @@ import org.scalatest.BeforeAndAfter
 import org.mockito.Mockito._
 import org.apache.log4j.Logger
 import uk.org.nbn.nbnv.importer.utility.ResourceLoader
+import collection.mutable.Map
 
 class ZipFileManagerSuite extends BaseFunSuite with BeforeAndAfter with ResourceLoader  {
   var zfm : ZipFileManager = _
+  var result = Map.empty[String, String]
 
   before{
     val logger = mock[Logger]
     zfm = new ZipFileManager(logger)
-  }
 
-  test("should extract valid zip") {
     val archive = resource("/archives/valid.zip")
 
-    val result = zfm.unZip(archive.getFile, "./temp")
-    result.length should be (3)
+    result = zfm.unZip(archive.getFile, "./temp")
+  }
+
+  test("should extract 3 elements") {
+    result.size should be (3)
+  }
+
+  test("should extract meta.xml") {
+    result.get("meta.xml") should not be (None)
+  }
+
+  test("should extract eml.xml") {
+    result.get("eml.xml") should not be (None)
+  }
+
+  test("should extract data.tab") {
+    result.get("data.tab") should not be (None)
   }
 }
