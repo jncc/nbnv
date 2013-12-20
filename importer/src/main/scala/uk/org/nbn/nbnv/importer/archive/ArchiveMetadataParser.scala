808a4978a4b47486fa33bec32b86413e4d0ea7e2
felix mason <felix.mason@jncc.gov.uk>
2013-12-20 15:12:13 +0000

    #nbnv-857 - finished metadata parsing

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/archive/ArchiveMetadataParser.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/archive/ArchiveMetadataParser.scala
@@ -8,43 +8,49 @@ import xml.{Elem, Node}
 class ArchiveMetadataParser  {
   def getMetadata(xml: Elem): ArchiveMetadata = {
 
-    def getFieldIndex(isCore: Boolean, term : String ): Option[Int] = {
-      val uri = "%s%s".format((if (isCore)
-        "http://rs.tdwg.org/dwc/terms/" else "http://rs.nbn.org.uk/dwc/nxf/0.1/terms/")
-        , term)
+    def getCoreField(term: String) = {
+      getFieldIndex( "http://rs.tdwg.org/dwc/terms/", term)
+    }
+
+    def getExtensionField(term: String) = {
+      getFieldIndex("http://rs.nbn.org.uk/dwc/nxf/0.1/terms/", term)
+    }
+
+    def getFieldIndex(uriPrefix: String, term : String ): Option[Int] = {
+      val uri = "%s%s".format(uriPrefix, term)
       val elem = ((xml \\ "field") filter {e => (e \ "@term").text == uri})
       if (elem.isEmpty) {
         None
       }
       else {
-        (elem.head \ "@index").text.maybeInt
+        Option((elem.head \ "@index").text.toInt)
       }
     }
 
     new ArchiveMetadata {
       val SkipHeaderLines: Option[Int] = (xml \ "core" \ "@ignoreHeaderLines").text.maybeInt
-      val date: Option[Int] = getFieldIndex(true, "eventDate")
-      val startDate: Option[Int] = getFieldIndex(true, "wibble")
-      val gridReferenceType: Option[Int] = None
-      val sensitiveOccurrence: Option[Int] = None
-      val siteName: Option[Int] = None
-      val key: Option[Int] = None
-      val gridReferencePrecision: Option[Int] = None
-      val determiner: Option[Int] = None
-      val recorder: Option[Int] = None
-      val siteKey: Option[Int] = None
-      val featureKey: Option[Int] = None
-      val attributes: Option[Int] = None
-      val srs: Option[Int] = None
-      val taxonVersionKey: Option[Int] = None
-      val north: Option[Int] = None
-      val dateType: Option[Int] = None
-      val endDate: Option[Int] = None
-      val gridReference: Option[Int] = None
-      val absence: Option[Int] = None
-      val surveyKey: Option[Int] = None
-      val east: Option[Int] = None
-      val sampleKey: Option[Int] = None
+      val date: Option[Int] = getCoreField("eventDate")
+      val startDate: Option[Int] = getExtensionField("eventDateStart")
+      val gridReferenceType: Option[Int] = getExtensionField("gridReferenceType")
+      val sensitiveOccurrence: Option[Int] = getExtensionField("sensitiveOccurrence")
+      val siteName: Option[Int] = getCoreField("locality")
+      val key: Option[Int] = getCoreField("occurrenceID")
+      val gridReferencePrecision: Option[Int] = getExtensionField("gridReferencePrecision")
+      val determiner: Option[Int] = getCoreField("identifiedBy")
+      val recorder: Option[Int] = getCoreField("recordedBy")
+      val siteKey: Option[Int] = getCoreField("locationID")
+      val featureKey: Option[Int] = getExtensionField("siteFeatureKey")
+      val attributes: Option[Int] = getCoreField("dynamicProperties")
+      val srs: Option[Int] = getCoreField("verbatimSRS")
+      val taxonVersionKey: Option[Int] = getCoreField("taxonID")
+      val north: Option[Int] = getCoreField("verbatimLatitude")
+      val dateType: Option[Int] = getExtensionField("eventDateTypeCode")
+      val endDate: Option[Int] = getExtensionField("eventDateEnd")
+      val gridReference: Option[Int] = getExtensionField("gridReference")
+      val absence: Option[Int] = getCoreField("occurrenceStatus")
+      val surveyKey: Option[Int] = getCoreField("collectionCode")
+      val east: Option[Int] = getCoreField("verbatimLongitude")
+      val sampleKey: Option[Int] = getCoreField("eventID")
     }
   }
 
