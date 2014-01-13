0d4d501b7097af81386e0948f984e612a994c787
felix mason <felix.mason@jncc.gov.uk>
2014-01-13 14:55:08 +0000

    #nbnv-857 - fixed a test yay

--- importer/src/test/scala/uk/org/nbn/nbnv/importer/ImporterSuite.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/ImporterSuite.scala
@@ -1,13 +1,13 @@
 package uk.org.nbn.nbnv.importer
 
-import archive.Archive
+import archive.{ArchiveMetadata, ArchiveFilePaths, Archive}
 import darwin.ArchiveManager
 import ingestion.Ingester
 import org.apache.log4j.Logger
 import org.mockito.Mockito._
 import org.mockito.Matchers._
 import testing.BaseFunSuite
-import uk.org.nbn.nbnv.importer.metadata.MetadataReader
+import metadata.{Metadata, MetadataReader}
 import javax.persistence.{EntityManager, EntityTransaction}
 import utility.Stopwatch
 import validation.Validator
@@ -18,10 +18,18 @@ class ImporterSuite extends BaseFunSuite  {
 
     // arrange
 
+    val archiveFiles = mock[ArchiveFilePaths]
+    when(archiveFiles.metadata).thenReturn("some/path")
+
     val log = mock[Logger]
+
+
     val archive = mock[Archive]
-    
+    when(archive.getArchiveFiles).thenReturn(archiveFiles)
+
+    val metadata = mock[Metadata]
     val metadataReader = mock[MetadataReader]
+    when(metadataReader.read("some/path")).thenReturn(metadata)
 
     val entityManager = mock[EntityManager]
     when(entityManager.getTransaction).thenReturn(mock[EntityTransaction])
