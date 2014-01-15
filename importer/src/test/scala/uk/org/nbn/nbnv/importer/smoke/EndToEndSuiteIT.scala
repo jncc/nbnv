1b2f88bf47762357e4b10c2bd55b7bf70575a0c3
felix mason <felix.mason@jncc.gov.uk>
2014-01-15 15:21:35 +0000

    #nbnv-875 - fixed issue with data file parser not be lazy

--- importer/src/test/scala/uk/org/nbn/nbnv/importer/smoke/EndToEndSuiteIT.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/smoke/EndToEndSuiteIT.scala
@@ -4,21 +4,12 @@ import java.io.File
 import uk.org.nbn.nbnv.importer.testing.BaseFunSuite
 import uk.org.nbn.nbnv.importer.utility.ResourceLoader
 import uk.org.nbn.nbnv.importer._
-import java.net.{URI, URL}
-
-
-import metadata.{MetadataParser, MetadataReader}
-import org.gbif.dwc.text.ArchiveFactory
-import scala.collection.JavaConversions._
-import com.google.common.base.Stopwatch
-import uk.org.nbn.nbnv.importer.records.NbnRecord
-import uk.org.nbn.nbnv.importer.validation.Validator
-import org.apache.log4j.Logger
-import data.{Repository, QueryCache, Database}
-import uk.org.nbn.nbnv.PersistenceUtility
+import archive._
+import java.net.URL
 import uk.org.nbn.nbnv.importer.BadDataException
 import uk.org.nbn.nbnv.utility.FileSystem
-
+import org.apache.log4j.Logger
+import uk.org.nbn.nbnv.importer.BadDataException
 
 class EndToEndSuiteIT extends BaseFunSuite with ResourceLoader {
 
@@ -32,11 +23,78 @@ class EndToEndSuiteIT extends BaseFunSuite with ResourceLoader {
 
   // change from 'ignore' to 'test' to run the importer against an archive within your IDE
   ignore("import an archive") {
-    val archive = new URL("file:///C://Working//nbnv-827//archive_NBNGatewa_28112013_121644projprecv3.zip")
-    val f = fixture(archive, Target.validate)
+    val archive = new URL("file:///C://Working//nbnv-877//archive_CBDC_17122013_103319_Private.zip")
+    val f = fixture(archive, Target.ingest)
     f.importer.run()
   }
 
+  ignore("data file parser") {
+    val pathUrl = new URL("file:///C://Working//testreader//archive_CBDC_17122013_103319_Private//data.tab2")
+    val mappingUrl = new URL("file:///C://Working//testreader//archive_CBDC_17122013_103319_Private//meta.xml")
+
+    val fs = new FileSystem
+    val xml = fs.loadXml(mappingUrl.getFile)
+    val parser = new ArchiveMetadataParser
+
+    val metadata = parser.getMetadata(xml)
+
+    val options = mock[Options]
+    val log = mock[Logger]
+    val crf = new CsvReaderFactory
+    val rf = new NbnRecordFactory(log)
+    val dfp = new DataFileParser(options, rf, log, crf)
+
+    dfp.open(pathUrl.getFile, metadata)
+
+    var index = 0
+
+
+
+    dfp.records.foreach(r => {
+      if (index == 0 || index % 100 == 0) println("%s, line %d".format(r.key, index))
+      index = index+1
+    })
+  }
+
+
+  ignore("csv reader") {
+//    val pathUrl = resource("/archives/valid/data.tab")
+    val pathUrl = new URL("file:///C://Working//testreader//archive_CBDC_17122013_103319_Private//data.tab2")
+    val mappingUrl = new URL("file:///C://Working//testreader//archive_CBDC_17122013_103319_Private//meta.xml")
+
+    val fs = new FileSystem
+    val xml = fs.loadXml(mappingUrl.getFile)
+    val parser = new ArchiveMetadataParser
+
+    val metadata = parser.getMetadata(xml)
+
+
+    val csvReader = new CsvReader(new File(pathUrl.getFile))
+
+    val log = mock[Logger]
+    val recordFac = new NbnRecordFactory(log)
+    var index = 0
+//    csvReader.drop(1).foreach(x => {
+//      val record = recordFac.makeRecord(x, metadata)
+//      if (index % 100 == 0) println("%s [%d] : length: %d".format(record.key, index, x.length))
+//      index = index+1
+//    })
+
+    val x = csvReader.drop(metadata.skipHeaderLines.getOrElse(0)).view.zipWithIndex.map({ case (rawData, i) =>
+      //      if (rawData.length < metadata.fields) {
+      //        log.warn("The record at row %d contains less fields then the %d mapped fields".format(i + 1,metadata.fields))
+      //      }
+
+      recordFac.makeRecord(rawData, metadata)
+    })
+
+
+
+    csvReader.drop(1).zipWithIndex.map{case (record, index) =>
+      val nbnrec = recordFac.makeRecord(record, metadata)
+      if (index == 0 || index % 100 == 0) println("%s [%d] : length: %d".format(nbnrec.key, index, record.length))
+    }
+  }
 //  ignore("Make reader sloooooowww") {
 //    val archiveURL = new URL("file:///C://Working//bwarstst//archive_BWARS_10000R.zip")
 //
