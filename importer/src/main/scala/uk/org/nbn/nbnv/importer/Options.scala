b20a437697451bf212052b0051c8edda8fcaee22
felix mason <felix.mason@jncc.gov.uk>
2014-01-22 15:31:03 +0000

    Added flush parameter to enable tuning of jpa

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/Options.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/Options.scala
@@ -1,5 +1,7 @@
 package uk.org.nbn.nbnv.importer
 
+import uk.org.nbn.nbnv.importer.utility.StringParsing._
+
 /// The result of trying to parse the command line options.
 sealed abstract class OptionsResult
 case class OptionsSuccess(options: Options) extends OptionsResult
@@ -13,7 +15,8 @@ case class Options(archivePath: String  = "specify-an-archive-path.zip",
                    target:      Target.Value = Target.commit,
                    logLevel:    String  = "INFO",
                    logDir:      String  = ".",
-                   tempDir:     String  = "./temp")
+                   tempDir:     String  = "./temp",
+                   flush:       Int = 100)
 {
   override def toString = {
     Map(
@@ -21,7 +24,8 @@ case class Options(archivePath: String  = "specify-an-archive-path.zip",
       "  target      %s" -> target,
       "  logLevel    %s" -> logLevel,
       "  logDir      %s" -> logDir,
-      "  tempDir     %s" -> tempDir)
+      "  tempDir     %s" -> tempDir,
+      "  flush       %d" -> flush)
       .map(x => x._1.format(x._2))
       .mkString("\n")
   }
@@ -31,12 +35,13 @@ object Options {
 
   val usage =
     """|
-       |Usage:
-       |importer "C:\some\archive.zip" (specify a DwcA archive)
-       |         [-target validate]    (specify validate|verify|ingest|commit)
-       |         [-logLevel DEBUG]     (specify a log level)
-       |         [-logDir "C:\logs"]   (specify a log directory)
-       |         [-tempDir "C:\temp"]  (specify a temp directory)
+      |Usage:
+      |importer "C:\some\archive.zip" (specify a DwcA archive)
+      |         [-target validate]    (specify validate|verify|ingest|commit)
+      |         [-logLevel DEBUG]     (specify a log level)
+      |         [-logDir "C:\logs"]   (specify a log directory)
+      |         [-tempDir "C:\temp"]  (specify a temp directory)
+      |         [-flush "100"]  (specify the number of records after which the persistence layer should flush)
     """.stripMargin
 
   /// Parses the command line arguments.
@@ -48,9 +53,9 @@ object Options {
       case "-logdir"   :: v :: tail => process(options.copy(logDir = v), tail)
       case "-loglevel" :: v :: tail => process(options.copy(logLevel = v), tail)
       case "-target"   :: v :: tail => process(options.copy(target = Target.withName(v.toLowerCase)), tail)
+      case "-flush"  :: v :: tail => process(options.copy(flush = v.maybeInt.getOrElse(100)), tail)
       case v                :: tail => process(options.copy(archivePath = v), tail)
     }
-
     args match {
       case Nil => OptionsFailure(usage)
       case _   => OptionsSuccess(process(Options(), args.map(_.toLowerCase)))
