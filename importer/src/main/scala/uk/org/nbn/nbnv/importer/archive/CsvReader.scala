2aa291a3c93c95d2b7be4d1995dd279b5ea05f60
felix mason <felix.mason@jncc.gov.uk>
2014-02-11 16:58:03 +0000

    Modified importer to use simple scala io csv file reading to avoid issues with double quotes

--- importer/src/main/scala/uk/org/nbn/nbnv/importer/archive/CsvReader.scala
+++ importer/src/main/scala/uk/org/nbn/nbnv/importer/archive/CsvReader.scala
@@ -1,29 +1,39 @@
 package uk.org.nbn.nbnv.importer.archive
 
 import java.io._
-import org.supercsv.io._
-import org.supercsv.prefs._
-import scala.collection.JavaConversions._
+
+import scala.io.Source
 
 class CsvReader(file:File) extends Iterable[List[String]]{
 
-  def iterator = new Iterator[List[String]]{
-    private lazy val prefs = new CsvPreference.Builder('"', '\t', "\r\n").build()
-    private lazy val reader = new CsvListReader(new FileReader(file),prefs)
-
-    private var current:List[String] = null
-
-    def next():List[String] = current
-    def hasNext():Boolean = {
-      val line = reader.read()
-      if(line == null){
-        reader.close
-        false
-      }
-      else{
-        current = line.toList
-        true
-      }
-    }
+  def iterator: Iterator[List[String]] = {
+    Source.fromFile(file).getLines().map(l => l.split("\t").toList)
   }
 }
+
+//
+//import org.supercsv.io._
+//import org.supercsv.prefs._
+//import scala.collection.JavaConversions._
+//class CsvReader(file:File) extends Iterable[List[String]]{
+//
+//  def iterator = new Iterator[List[String]]{
+//    private lazy val prefs = new CsvPreference.Builder('"','\t', "\r\n").build()
+//    private lazy val reader = new CsvListReader(new FileReader(file),prefs)
+//
+//    private var current:List[String] = null
+//
+//    def next():List[String] = current
+//    def hasNext():Boolean = {
+//      val line = reader.read()
+//      if(line == null){
+//        reader.close
+//        false
+//      }
+//      else{
+//        current = line.toList
+//        true
+//      }
+//    }
+//  }
+//}
--- importer/src/test/scala/uk/org/nbn/nbnv/importer/archive/NbnRecordFactorySuite.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/archive/NbnRecordFactorySuite.scala
@@ -8,6 +8,36 @@ import org.apache.log4j.Logger
 class NbnRecordFactorySuite extends BaseFunSuite with BeforeAndAfter {
   var factory : NbnRecordFactory = _
 
+  def fixture() = new {
+    val metadata = new ArchiveMetadata {
+      val date: Option[Int] = Some(22)
+      val startDate: Option[Int] = Some(3)
+      val gridReferenceType: Option[Int] = Some(10)
+      val sensitiveOccurrence: Option[Int] = Some(7)
+      val siteName: Option[Int] = Some(9)
+      val key: Option[Int] = Some(0)
+      val gridReferencePrecision: Option[Int] = Some(12)
+      val skipHeaderLines: Option[Int] = None
+      val determiner: Option[Int] = Some(14)
+      val recorder: Option[Int] = Some(13)
+      val featureKey: Option[Int] = Some(23)
+      val siteKey: Option[Int] = Some(8)
+      val fields: Int = 24
+      val attributes: Option[Int] = Some(18)
+      val srs: Option[Int] = Some(19)
+      val fieldSeparator: String = "\\t"
+      val taxonVersionKey: Option[Int] = Some(6)
+      val north: Option[Int] = Some(21)
+      val dateType: Option[Int] = Some(5)
+      val endDate: Option[Int] = Some(4)
+      val gridReference: Option[Int] = Some(11)
+      val absence: Option[Int] = Some(24)
+      val surveyKey: Option[Int] = Some(1)
+      val east: Option[Int] = Some(20)
+      val sampleKey: Option[Int] = Some(2)
+    }
+  }
+
   before {
     val log = mock[Logger]
 
@@ -15,6 +45,8 @@ class NbnRecordFactorySuite extends BaseFunSuite with BeforeAndAfter {
   }
 
   test("should parse a record") {
+    val f = fixture()
+
     val rawRecord = List(
       "CI00000300000TNR",
       "CI0000030000000A",
@@ -43,35 +75,7 @@ class NbnRecordFactorySuite extends BaseFunSuite with BeforeAndAfter {
       "absence"
     )
 
-    val metadata = new ArchiveMetadata {
-      val date: Option[Int] = Some(22)
-      val startDate: Option[Int] = Some(3)
-      val gridReferenceType: Option[Int] = Some(10)
-      val sensitiveOccurrence: Option[Int] = Some(7)
-      val siteName: Option[Int] = Some(9)
-      val key: Option[Int] = Some(0)
-      val gridReferencePrecision: Option[Int] = Some(12)
-      val skipHeaderLines: Option[Int] = None
-      val determiner: Option[Int] = Some(14)
-      val recorder: Option[Int] = Some(13)
-      val featureKey: Option[Int] = Some(23)
-      val siteKey: Option[Int] = Some(8)
-      val fields: Int = 24
-      val attributes: Option[Int] = Some(18)
-      val srs: Option[Int] = Some(19)
-      val fieldSeparator: String = "\\t"
-      val taxonVersionKey: Option[Int] = Some(6)
-      val north: Option[Int] = Some(21)
-      val dateType: Option[Int] = Some(5)
-      val endDate: Option[Int] = Some(4)
-      val gridReference: Option[Int] = Some(11)
-      val absence: Option[Int] = Some(24)
-      val surveyKey: Option[Int] = Some(1)
-      val east: Option[Int] = Some(20)
-      val sampleKey: Option[Int] = Some(2)
-    }
-
-    var record = factory.makeRecord(rawRecord, metadata)
+    var record = factory.makeRecord(rawRecord, f.metadata)
 
     record.eventDateRaw should be (Some("13/07/2001"))
     record.startDateRaw should be (Some("19/07/2001"))
@@ -107,6 +111,8 @@ class NbnRecordFactorySuite extends BaseFunSuite with BeforeAndAfter {
 
   test("record factory should return None for unparsable data")
   {
+    val f = fixture()
+
     val rawRecord = List(
       "CI00000300000TNR",
       "CI0000030000000A",
@@ -135,35 +141,7 @@ class NbnRecordFactorySuite extends BaseFunSuite with BeforeAndAfter {
       "absence"
     )
 
-    val metadata = new ArchiveMetadata {
-      val date: Option[Int] = Some(22)
-      val startDate: Option[Int] = Some(3)
-      val gridReferenceType: Option[Int] = Some(10)
-      val sensitiveOccurrence: Option[Int] = Some(7)
-      val siteName: Option[Int] = Some(9)
-      val key: Option[Int] = Some(0)
-      val gridReferencePrecision: Option[Int] = Some(12)
-      val skipHeaderLines: Option[Int] = None
-      val determiner: Option[Int] = Some(14)
-      val recorder: Option[Int] = Some(13)
-      val featureKey: Option[Int] = Some(23)
-      val siteKey: Option[Int] = Some(8)
-      val fields: Int = 24
-      val attributes: Option[Int] = Some(18)
-      val srs: Option[Int] = Some(19)
-      val fieldSeparator: String = "\\t"
-      val taxonVersionKey: Option[Int] = Some(6)
-      val north: Option[Int] = Some(21)
-      val dateType: Option[Int] = Some(5)
-      val endDate: Option[Int] = Some(4)
-      val gridReference: Option[Int] = Some(11)
-      val absence: Option[Int] = Some(24)
-      val surveyKey: Option[Int] = Some(1)
-      val east: Option[Int] = Some(20)
-      val sampleKey: Option[Int] = Some(2)
-    }
-
-    var record = factory.makeRecord(rawRecord, metadata)
+    var record = factory.makeRecord(rawRecord, f.metadata)
 
     record.east should be (None)
     record.eastRaw should be (Some("badEast"))
@@ -179,6 +157,48 @@ class NbnRecordFactorySuite extends BaseFunSuite with BeforeAndAfter {
 
     record.gridReferencePrecision should be (None)
     record.gridReferencePrecisionRaw should be (Some("badPrecision"))
+
+    record.siteName should be (Some("Sherwood Forest Holiday Village: lake 4"))
+
+    record.gridReferenceTypeRaw should be (None)
+  }
+
+  test("should handle quoted data correctly") {
+    val f = fixture()
+
+    val rawRecord = List(
+      "CI00000300000TNR",
+      "CI0000030000000A",
+      "CI000003000003T3",
+      "19/07/2001",
+      "21/07/2001",
+      "D",
+      "NBNSYS0000006640",
+      "badOccurrence",
+      "CI0000030000034X",
+      "\"Sherwood Forest Holiday Village: lake 4\"",
+      "\"\"",
+      "",
+      "badPrecision",
+      "Robert Merritt",
+      "Robert Merritt dt",
+      "Field Observation",
+      "",
+      "",
+      "{\"Abundance\":\"\",\"Comment\":\"\",\"SampleMethod\":\"Field Observation\"}",
+      "badSRS",
+      "badEast",
+      "badNorth",
+      "13/07/2001",
+      "FeatureKeyTest",
+      "absence"
+    )
+
+    var record = factory.makeRecord(rawRecord, f.metadata)
+
+    record.siteName should be (Some("Sherwood Forest Holiday Village: lake 4"))
+
+    record.gridReferenceTypeRaw should be (None)
   }
 
 
