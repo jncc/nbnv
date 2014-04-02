b2451c758202caddde9ac630bc9cfcfeb9add6b0
Matt Debont <matt.debont@devlag.org>
2014-04-02 09:53:53 +0100

    Removed one of the database calls while downloading data, we don't need to grab what attributes we have and then grab the attributes for the records, now we can just grab the attributes for the records and use the streaming to get the attribute ids out

--- api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/utils/TaxonObservationAttributeHandler.java
+++ api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/utils/TaxonObservationAttributeHandler.java
@@ -4,8 +4,12 @@
  */
 package uk.org.nbn.nbnv.api.rest.resources.utils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.apache.ibatis.session.ResultContext;
 import org.apache.ibatis.session.ResultHandler;
 import uk.org.nbn.nbnv.api.model.TaxonObservationAttribute;
@@ -17,18 +21,26 @@ import uk.org.nbn.nbnv.api.model.TaxonObservationAttribute;
 public class TaxonObservationAttributeHandler implements ResultHandler {
 
     private Map<Integer, Map<Integer, String>> observationAttributes;
+    private Set<Integer> attributeIDs;
     
     public TaxonObservationAttributeHandler() {
         observationAttributes = new HashMap<Integer, Map<Integer, String>>();
+        attributeIDs = new HashSet<Integer>();
     }
     
     public Map<Integer, Map<Integer, String>> getObservationAttributes() {
         return observationAttributes;
     }
     
+    public List<Integer> getAttributeIDs() {
+        return new ArrayList<Integer>(attributeIDs);
+    }
+    
     @Override
     public void handleResult(ResultContext rc) {
         TaxonObservationAttribute attribute = (TaxonObservationAttribute) rc.getResultObject();
+        
+        attributeIDs.add(attribute.getAttributeID());
         
         // Put attributes into a map structure for retrieval later
         // Map --> obsID --> Map --> attID --> attVal
