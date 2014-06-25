1ca421ce63bc485dffb9c86d884acdcda4ce276d
felix mason <felix.mason@jncc.gov.uk>
2014-06-25 16:54:34 +0100

    #nbnv-989 - GSI factory should return none for lat lngs that fall in a grid square polygon but don't convert to a valid grid ref. (cherry picked from commit e52008fd44de20846418381e7448ed0c91d504b9)

--- importer/src/test/scala/uk/org/nbn/nbnv/importer/smoke/EndToEndSuiteIT.scala
+++ importer/src/test/scala/uk/org/nbn/nbnv/importer/smoke/EndToEndSuiteIT.scala
@@ -23,7 +23,7 @@ class EndToEndSuiteIT extends BaseFunSuite with ResourceLoader {
 
   // change from 'ignore' to 'test' to run the importer against an archive within your IDE
   ignore("import an archive") {
-    val archive = new URL("file:///C://Working//nbnv-929//archive_DBRC_28022014_122140.zip")
+    val archive = new URL("file:///C://Working//nbnv-989//archive_MBA_GA000941_12052014_045507ShoreThing.zip")
     val f = fixture(archive, Target.validate)
     f.importer.run()
   }
