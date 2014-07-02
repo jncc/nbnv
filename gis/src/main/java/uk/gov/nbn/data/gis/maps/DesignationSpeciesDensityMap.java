2bd3c6c12af826d138606f8de4a2a346dc096c49
Matt Debont <matt.debont@devlag.org>
2014-07-02 13:40:17 +0100

    Allow Spaces in Designations in the GIS Designation Species Map

--- gis/src/main/java/uk/gov/nbn/data/gis/maps/DesignationSpeciesDensityMap.java
+++ gis/src/main/java/uk/gov/nbn/data/gis/maps/DesignationSpeciesDensityMap.java
@@ -93,7 +93,7 @@ public class DesignationSpeciesDensityMap {
             @RequestParam(value = "datasets", required=false) @Datasets final List<String> datasetKeys,
             @RequestParam(value = "startyear", required=false) @Pattern(regexp="[0-9]{4}") final String startYear,
             @RequestParam(value = "endyear", required=false) @Pattern(regexp="[0-9]{4}") final String endYear,
-            @PathVariable("designationKey") @Pattern(regexp="^[A-Z0-9.()/_\\-]+$") final String key) {
+            @PathVariable("designationKey") @Pattern(regexp="^[A-Z0-9.()/_\\- ]+$") final String key) {
 
         HashMap<String, Object> data = new HashMap<String, Object>();
         data.put("layers", LAYERS.keySet());
