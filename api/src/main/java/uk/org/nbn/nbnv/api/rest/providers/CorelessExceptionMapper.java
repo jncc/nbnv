69bb0bbf9887aaa47d16524e9a76fea91f13eb20
Matt Debont <matt.debont@jncc.gov.uk>
2014-10-16 15:01:45 +0100

    Have an operational coreless running feature that swaps out the core datasource with a placeholder that only throws errors after it has been set up, have the api grabbing mybatis level errors in an exception mapper, not ideal but so far the prettiest way to deal with this issue, also can prevent horrible sql stack traces showing up.

---
+++ api/src/main/java/uk/org/nbn/nbnv/api/rest/providers/CorelessExceptionMapper.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.org.nbn.nbnv.api.rest.providers;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import org.mybatis.spring.MyBatisSystemException;
+import org.springframework.stereotype.Repository;
+import uk.org.nbn.nbnv.api.rest.resources.utils.throwables.CorelessException;
+
+/**
+ *
+ * @author Matt Debont
+ */
+@Provider
+@Repository
+public class CorelessExceptionMapper implements ExceptionMapper<MyBatisSystemException> {
+
+    @Override
+    public Response toResponse(MyBatisSystemException e) {
+        Map<String, Object> toReturn = new HashMap<String, Object>();
+        toReturn.put("success", false);
+        toReturn.put("status", e.getMessage());
+        
+        return Response.status(Response.Status.BAD_REQUEST)
+            .type(MediaType.APPLICATION_JSON)
+            .entity(toReturn)
+            .build();
+    }
+}
---
+++ api/src/main/java/uk/org/nbn/nbnv/api/rest/resources/utils/datasource/CoreDatasource.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.org.nbn.nbnv.api.rest.resources.utils.datasource;
+
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+
+/**
+ *
+ * @author Matt Debont
+ */
+public class CoreDatasource extends TransactionAwareDataSourceProxy {
+    private static Logger logger = LoggerFactory.getLogger(CoreDatasource.class);
+    
+    public CoreDatasource(DataSource datasource, boolean coreless) {
+        super(coreless ? new PlaceholderDatasource() : datasource);
+        logger.warn("Coreless datasource is " + coreless + " --------------------------------------------------------");
+    }
+}
