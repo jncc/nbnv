94ed62c04b6c48ea4f0a78864b8aebba23144ca9
Matt Debont <matt.debont@jncc.gov.uk>
2015-07-24 10:45:50 +0100

    Removed references to DataLicenceData, not needed and the table will no longer exist when this is deployed

--- api.crud/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/DatasetLicenceMapper.java
+++ api.crud/src/main/java/uk/org/nbn/nbnv/api/dao/warehouse/DatasetLicenceMapper.java
@@ -8,20 +8,20 @@ import uk.org.nbn.nbnv.api.model.DatasetLicence;
 public interface DatasetLicenceMapper {
 
     @Select("SELECT CASE WHEN img IS NULL THEN 0 ELSE 1 END as hasImg, id, "
-            + "abbreviation, name, summary, href FROM DatasetLicenceData WHERE "
+            + "abbreviation, name, summary, href FROM DatasetLicence WHERE "
             + "id = #{id}")
     DatasetLicence getDatasetLicenceByID(@Param("id") int id);    
     
     @Select("SELECT CASE WHEN img IS NULL THEN 0 ELSE 1 END as hasImg, id, "
-            + "abbreviation, name, summary, href FROM DatasetLicenceData WHERE "
+            + "abbreviation, name, summary, href FROM DatasetLicence WHERE "
             + "abbreviation = #{abbrv}")
     DatasetLicence getDatasetLicenceByAbbrv(@Param("abbrv") String abbrv);
     
     @Select("SELECT CASE WHEN img IS NULL THEN 0 ELSE 1 END as hasImg, id, "
-            + "abbreviation, name, summary, href FROM DatasetLicenceData "
+            + "abbreviation, name, summary, href FROM DatasetLicence "
 			+ "ORDER BY name")
     List<DatasetLicence> selectAllDatasetLicences();
     
-    @Select("SELECT img FROM DatasetLicenceData WHERE id = #{id}")
+    @Select("SELECT img FROM DatasetLicence WHERE id = #{id}")
     Object selectImgByDatasetLicenceDataID(int id);
 }
